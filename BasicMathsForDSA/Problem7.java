4dd77a48f610f04a216d6aa1e943eda9a6ddd166
Shresha Acharya <shreshacharya06@gmail.com>
2024-10-08 17:40:35 +0530

    GCD LCM

---
+++ BasicMathsForDSA/Problem7.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+class Solution {
+    public Long[] lcmAndGcd(Long A , Long B) {
+        // code here
+        long gcd=gcd(A,B);
+        long lcm=(A*B)/gcd;
+        
+        return new Long[] {lcm,gcd};
+  
+    }
+    
+public long gcd(long x, long y){
+      if(x==0) return y;
+        if(y==0) return x;
+        if(x>y) return gcd(x%y,y);
+        else{
+          return  gcd(y%x,x);
+            
+        }
+     
+}
+
+};
+public class Problem7 {
+    public static void main(String[] args) {
+        Solution s= new Solution();
+        Long[] result=s.lcmAndGcd(10L,6L);
+        
+      System.out.println(Arrays.toString(result));  
+    }
+}
