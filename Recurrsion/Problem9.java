c0e787b5c54aa129fb189b2fda500fdb6ffc371e
Shresha Acharya <shreshacharya06@gmail.com>
2024-10-11 11:23:48 +0530

    Sub sequence

---
+++ Recurrsion/Problem9.java
@@ -0,0 +1,31 @@
+import java.util.List;
+import java.util.ArrayList;
+class Solution {
+    public int index=0;
+    
+    public List<List<Integer>> subset= new ArrayList<>();
+
+     public List<List<Integer>> subsets(int[] nums) {
+        f(0,new ArrayList<Integer>(),nums);
+            return subset; 
+         }
+ 
+     public void f(int index,ArrayList<Integer> arr,int[] nums){
+         if(index>=nums.length){
+             subset.add(new ArrayList<>(arr));
+             return;
+         }
+         arr.add(nums[index]);
+         f(index+1,arr,nums);
+         arr.remove(arr.size()-1);
+         f(index+1,arr,nums);
+     }
+     }
+public class Problem9 {
+    public static void main(String[] args) {
+     Solution s=new Solution();
+     int[] num={3,1,2};
+    System.out.println(s.subsets(num)); 
+    }
+    
+}
