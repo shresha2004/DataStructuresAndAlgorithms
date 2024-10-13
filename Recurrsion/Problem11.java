633ff3e2b96e5abe3da29868786f8aefe96339b5
Shresha Acharya <shreshacharya06@gmail.com>
2024-10-13 11:44:27 +0530

    Subsequence pattern

--- Recurrsion/Problem11.java
+++ Recurrsion/Problem11.java
@@ -6,34 +6,42 @@ class Solution{
     int i=0;
     int sum=0;
 
-    void f(int index,ArrayList<Integer> arr,int[] GivenArray){
-        if(index>=GivenArray.length){
-            
+    boolean f(int index,ArrayList<Integer> arr,int[] GivenArray){
+        if(index==GivenArray.length){
+            subs.add(arr);
             if(sum==4){
                 System.out.println(arr);
-            }
-            subs.add(arr);
-            return;
+                return true;
+            }else{return false;}
+            
+            
         }
         arr.add(GivenArray[index]);
     
         sum+=GivenArray[index];
-        f(index+1,new ArrayList<>(arr),GivenArray);
+     if(f(index+1,new ArrayList<>(arr),GivenArray)==true){
+        return true;
+     };
         
         arr.remove((arr.size())-1);
         sum-=GivenArray[index];
-        f(index+1,new ArrayList<>(arr),GivenArray);
+
+       if( f(index+1,new ArrayList<>(arr),GivenArray)==true){
+        return true;
+       };
+       return false;
     }
     
 
 }
-public class Problem10 {
+public class Problem11 {
     public static void main(String[] args) {
         Solution s=new Solution();
-        int[] arr={3,1,2};
-        s.f(0, new ArrayList<>(), arr);
+        int[] arr={2,2,3,1};
+       System.out.println(s.f(0, new ArrayList<>(), arr)); 
        //System.out.println(s.subs);
         
     }
     
 }
+
