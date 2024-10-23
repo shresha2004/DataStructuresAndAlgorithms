2b3adf04f91b6fcacbd2ed0758e52b374bebab00
Shresha Acharya <shreshacharya06@gmail.com>
2024-10-23 19:30:47 +0530

    Search algos

---
+++ Searching/LinearSearch.java
@@ -0,0 +1,22 @@
+//Problem:https://www.geeksforgeeks.org/problems/who-will-win-1587115621/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=who-will-win
+
+class Solution {
+    static boolean searchInSorted(int arr[], int k) {
+       
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==k){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+public class LinearSearch {
+    public static void main(String args[]){
+        Solution s=new Solution();
+        int[] arr ={1, 2, 3, 4, 6};
+        System.out.println(s.searchInSorted(arr,6));
+    }
+    
+    
+}
