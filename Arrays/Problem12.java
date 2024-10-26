6d430335c8a327c958ec1af0eead6bebe3b3cd56
Shresha Acharya <shreshacharya06@gmail.com>
2024-10-26 19:31:10 +0530

    Two sum

--- Arrays/Problem12.java
+++ Arrays/Problem12.java
@@ -1,7 +1,11 @@
 //Problem:https://leetcode.com/problems/two-sum/description/
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Collections;
+import java.util.ArrayList;
 class Solution{
+
+
     //Brute Force
     public int[] twoSumBruteForce(int[] nums,int target){
         int n=nums.length;
@@ -18,6 +22,45 @@ class Solution{
         return new int[0];
     }
 
+
+   //Better Solution(Without using hash map)
+    public int[] twoSumBetter(int[] nums,int target){
+    int n=nums.length;
+    int j=0;
+    //Selection sort
+    for(int i=0;i<n-1;i++){
+    int min=i;
+    for( j=i+1;j<n;j++){
+        if(nums[j]<nums[min]){
+            min=j;
+        }
+    }
+    if(min!=i){
+        int temp=nums[i];
+        nums[i]=nums[min];
+        nums[min]=temp;
+
+    }
+    }
+    System.out.println(Arrays.toString(nums));
+  int left=0;
+  int right=n-1;
+  int i=0;
+  while(i<n){
+    int check=nums[left]+nums[right];
+    if(check==target){
+        return new int[] {left,right};
+    }
+    if(check>target){
+        right--;
+    }
+    if(check<target){
+        left++;
+    }
+  }
+
+return new int[0];
+   }
 
     //Optimal Solution
     public int[] twoSumOptimal(int[] nums, int target) {
@@ -40,9 +83,10 @@ public class Problem12 {
     
     public static void main(String[] args) {
         Solution s=new Solution();
-        int[] nums ={2,7,11,15};
+        int[] nums ={2,11,7,15};
         int target=9;
        System.out.println("Brute Force:"+Arrays.toString( s.twoSumBruteForce(nums, target)));
+       System.out.println("Better:"+Arrays.toString( s.twoSumBetter(nums, target)));
        System.out.println("Optimal:"+Arrays.toString( s.twoSumOptimal(nums, target)));
 
     }
