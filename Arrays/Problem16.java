cf11e115a85e331b50ad9f518e7a357d04c1dfff
Shresha Acharya <shreshacharya06@gmail.com>
2024-10-31 18:13:25 +0530

    Kadane's Algorithm

---
+++ Arrays/Problem16.java
@@ -0,0 +1,48 @@
+//Problem:https://leetcode.com/problems/maximum-subarray/description/
+class Solution {
+    //BruteForce
+    public int maxSubArrayBruteForce(int[] nums) {
+        int n=nums.length;
+        int i,j;
+        int maxSum=nums[0];
+        int sum=0;
+        for(i=0;i<n;i++){
+            sum=0;
+            for(j=i;j<n;j++){
+                sum+=nums[j];
+                 maxSum=maxEle(sum,maxSum);
+            }
+           
+        }
+        return maxSum;
+        
+    }
+
+    //Kadane's Algorithm (Optimal)
+    public int maxSubArrayOptimal(int[] nums) {
+        int n=nums.length;
+        int i;
+        int maxSum=nums[0];
+        int sum=0;
+        for(i=0;i<n;i++){
+            sum+=nums[i];
+            maxSum=maxEle(sum, maxSum);
+            if(sum<0) sum=0;
+        }
+        return maxSum;
+        
+    }
+
+    public int maxEle(int a,int b){
+        if(a>b) return a;
+        return b;
+    }
+}
+public class Problem16 {
+    public static void main(String[] args) {
+        Solution s=new Solution();
+        int[] nums ={-2,1,-3,4,-1,2,1,-5,4};
+        System.out.println(s.maxSubArrayBruteForce(nums));
+        System.out.println(s.maxSubArrayOptimal(nums));
+    }
+}
