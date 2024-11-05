391ee7fff4b74ff1e98bb16b0713d74d6bd1fb89
Shresha Acharya <shreshacharya06@gmail.com>
2024-11-05 19:06:57 +0530

    Merge sorted array

--- Arrays/Problem18.java
+++ Arrays/Problem18.java
@@ -7,7 +7,7 @@ class Solution {
             nums1[i]=nums2[j];
             j++;
         }
-        System.out.println(Arrays.toString(nums1));
+       
         for(int i=0;i<size-1;i++){
            int min=i;
             for(int k=i;k<size;k++){
@@ -24,6 +24,29 @@ class Solution {
         }
         return nums1;
     }
+    public int[] mergeOptimal(int[] nums1, int m, int[] nums2, int n) {
+        int p=m+n-1;
+        int p1=m-1;
+        int p2=n-1;
+
+        while(p1>=0 && p2>=0){
+            if(nums1[p1]>nums2[p2]){
+                nums1[p]=nums1[p1];
+                p1--;
+            }else {
+                nums1[p]=nums2[p2];
+                p2--;
+            }
+            p--;
+        }
+
+        while(p2>=0){
+            nums1[p]=nums2[p2];
+            p--;
+            p2--;
+        }
+        return nums1;
+    }
 }
 
 public class Problem18 {
@@ -34,6 +57,6 @@ public class Problem18 {
        int n = 3;
         Solution s= new Solution();
         System.out.println("BruteForce:"+Arrays.toString(s.mergeBruteForce(nums1,m,nums2,n)));
-      //  System.out.println("Optimal:"+Arrays.toString(s.plusOneOptimal(digits)));
+      System.out.println("Optimal:"+Arrays.toString(s.mergeOptimal(nums1,m,nums2,n)));
     }
 }
