d34c4ceb8603662d8c0bc634afb6e6629a7a4513
Shresha Acharya <shreshacharya06@gmail.com>
2024-11-08 22:38:39 +0530

    Re arrange array

--- Arrays/Problem21.java
+++ Arrays/Problem21.java
@@ -23,11 +23,30 @@ class Solution {
         }
         return nums;
     }
+    public int[] rearrangeArrayBetter(int[] nums) {
+        int n=nums.length;
+        int[] arr=new int[n];
+        int even=0;
+        int odd=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]>0){
+                arr[even*2]=nums[i];
+                 even++;
+                
+            }
+            if(nums[i]<0){
+                arr[odd*2+1]=nums[i];
+               odd++;
+            }
+        }
+        return arr;
+    }
 }
 public class Problem21 {
     public static void main(String[] args) {
         Solution s=new Solution();
        int[] nums = {3,1,-2,-5,2,-4};
       System.out.println("BruteForce:"+Arrays.toString(s.rearrangeArrayBruteForce(nums)));
+      System.out.println("Better:"+Arrays.toString(s.rearrangeArrayBetter(nums)));
     }
 }
