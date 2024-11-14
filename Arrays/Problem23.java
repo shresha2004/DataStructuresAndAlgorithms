bba7702434d35effbc8d6f23baa91093118afa0c
Shresha Acharya <shreshacharya06@gmail.com>
2024-11-14 22:52:35 +0530

    Summary Ranges

--- Arrays/Problem23.java
+++ Arrays/Problem23.java
@@ -2,7 +2,7 @@
 import java.util.List;
 import java.util.ArrayList;
 class Solution {
-    public List<String> summaryRangesBruteForce(int[] nums) {
+    public List<String> summaryRangesOptimal(int[] nums) {
         List<String> ls= new ArrayList<>();
         int n=nums.length;
         int flag=0;
@@ -27,12 +27,29 @@ class Solution {
         if(flag2==0) ls.add(Integer.toString(nums[n-1]));
         return ls;
     }
+    public List<String> summaryRangesBruteForce(int[] nums) {
+        List<String> ls= new ArrayList<>();
+        if(nums == null || nums.length==0) return ls;
+        int start=0;
+        for(int i=1;i<=nums.length;i++){
+            if(i==nums.length || nums[i] != nums[i-1] +1){
+                if(start == i-1){
+                    ls.add(String.valueOf(nums[start]));
+                }else{
+                    ls.add(nums[start] + "->" + nums[i - 1]);
+                }
+                start=i;
+            }
+        }
+        return ls;
+    }
 }
 public class Problem23 {
     public static void main(String[] args) {
         Solution s=new Solution();
        int[] nums = {0,1,2,4,5,7};
       System.out.println("BruteForce Approach:"+s.summaryRangesBruteForce(nums));
+      System.out.println("Optimal Approach:"+s.summaryRangesOptimal(nums));
      
     }
 }
