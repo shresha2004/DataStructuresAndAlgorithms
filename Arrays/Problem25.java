130b7efc5a122b9d4daa2cc1c1ce0baebce1b980
Shresha Acharya <shreshacharya06@gmail.com>
2024-11-16 23:23:13 +0530

    Power of number and negative number

---
+++ Arrays/Problem25.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+class Solution {
+    double pow=1;
+    double ans=1;
+    public double myPowOptimal(double x, int n) {
+        long N=n;
+       if(N==0) return 1;
+       if(N<0){
+        x=1/x;
+        N = -N;
+       }
+       double half=myPowOptimal(x, (int) (N/2));
+       if(N%2==0){
+        return half*half;
+       }else
+       {
+       return half*half*x;
+       }
+    }
+}
+public class Problem25 {
+     public static void main(String[] args) {
+        Solution s=new Solution();
+       
+      System.out.println("Optimal Approach:"+s.myPowOptimal(2,10));
+      System.out.println("Optimal Approach:"+s.myPowOptimal(2,-3));
+     
+     
+    }
+}
