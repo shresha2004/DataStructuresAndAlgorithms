b6b197724c49bc9bf08117deea3dd8895db4dedc
Shresha Acharya <shreshacharya06@gmail.com>
2024-11-18 23:11:35 +0530

    Leaders Problem

--- Arrays/Problem25.java
+++ Arrays/Problem25.java
@@ -27,6 +27,7 @@ public class Problem25 {
         Solution s=new Solution();
        int[] nums = {16, 17, 4, 3, 5, 2};
       System.out.println("BruteForce Approach:"+s.leadersBruteForce(nums));
+      System.out.println("Optimal Approach:"+s.leadersBruteForce(nums));
      
      
     }
