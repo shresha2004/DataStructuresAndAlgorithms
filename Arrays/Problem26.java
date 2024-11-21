65a7806369dd9a029eac8df8f55471b1ad6fbc68
Shresha Acharya <shreshacharya06@gmail.com>
2024-11-21 23:00:07 +0530

    Longest Successive Element

--- Arrays/Problem26.java
+++ Arrays/Problem26.java
@@ -38,6 +38,42 @@ class Solution {
                 }
         return maxDuplicate+1;
     }
+    
+    public static int longestSuccessiveElementsOptimal(int []a) {
+        // Write your code here.
+        int n=a.length;
+        boolean swapped;
+        int duplicate=0;
+        int maxDuplicate=0;
+        for(int i=n-1;i>=0;i--){
+           // swapped=false;
+            for(int j=0;j<n-1;j++){
+            if(a[j+1]<a[j]){
+                int temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+              //  swapped=true;
+            }
+
+            }
+            if(i<n-1 && i>=1){
+            int check= a[i+1]-a[i];  
+            if(check==1  ){ 
+                duplicate++;
+                }
+            if(check !=0 && check !=1 || i==0){
+                 maxDuplicate=Math.max(maxDuplicate,duplicate);
+                 duplicate=0;
+            
+            }
+            } 
+
+           // if(!swapped) break;
+        }
+       
+        
+        return maxDuplicate+1;
+    }
 }
 public class Problem26 {
     public static void main(String[] args) {
@@ -47,9 +83,9 @@ public class Problem26 {
         198, 20, 78, 73, 111, 90, 152, 49, 6, 121, 70, 158, 3, 135, 148, 11, 155,
         166, 45, 187, 83, 39, 36, 14, 35, 70, 142, 149, 182, 96, 134, 192, 105,
         42, 16, 118, 137, 107, 50, 5, 186, 101, 96, 18, 166, 168, 200, 28, 178,
-        23, 53, 20, 135, 23, 16, 103         };
+        23, 53, 20, 135, 23, 16, 103};
       System.out.println("BruteForce Approach:"+s.longestSuccessiveElementsBruteForce(nums));
-    // System.out.println("Optimal Approach:"+s.leadersOptimal(nums));
+     System.out.println("Optimal Approach:"+s.longestSuccessiveElementsOptimal(nums));
  
     }
     
