1dd715bb7af3d995bd65c1caeae7eeefda327968
Shresha Acharya <shreshacharya06@gmail.com>
2024-11-22 22:36:00 +0530

    Longest Successive Element

--- Arrays/Problem26.java
+++ Arrays/Problem26.java
@@ -1,6 +1,7 @@
  //Problem:https://www.naukri.com/code360/problems/longest-successive-elements_6811740?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_Arrayproblems
 
 import java.util.Arrays;
+import java.util.HashSet;
 
 class Solution {
     public static int longestSuccessiveElementsBruteForce(int []a) {
@@ -42,49 +43,33 @@ class Solution {
     public static int longestSuccessiveElementsOptimal(int []a) {
         // Write your code here.
         int n=a.length;
-        boolean swapped;
-        int duplicate=0;
-        int maxDuplicate=0;
-        for(int i=n-1;i>=0;i--){
-           // swapped=false;
-            for(int j=0;j<n-1;j++){
-            if(a[j+1]<a[j]){
-                int temp=a[j];
-                a[j]=a[j+1];
-                a[j+1]=temp;
-              //  swapped=true;
+        int length=Integer.MIN_VALUE;
+        if(n==0) return 0;
+        HashSet<Integer> set =new HashSet<>();
+        for(int ele : a){
+            set.add(ele);
+        }
+        for(int ele : set){
+        if(!set.contains(ele-1)){
+            int cnt=1;
+           
+            int x=ele;
+            while(set.contains(x+1)){
+                x=x+1;
+                cnt++;
             }
-
-            }
-            if(i<n-1 && i>=1){
-            int check= a[i+1]-a[i];  
-            if(check==1  ){ 
-                duplicate++;
-                }
-            if(check !=0 && check !=1 || i==0){
-                 maxDuplicate=Math.max(maxDuplicate,duplicate);
-                 duplicate=0;
-            
-            }
-            } 
-
-           // if(!swapped) break;
+            length=Math.max(cnt,length);
         }
        
-        
-        return maxDuplicate+1;
+        }
+        return length;
     }
 }
 public class Problem26 {
     public static void main(String[] args) {
         Solution s=new Solution();
-       int[] nums = {
-        147, 25, 56, 168, 106, 124, 137, 117, 15, 45, 189, 92, 74, 46, 63, 20,
-        198, 20, 78, 73, 111, 90, 152, 49, 6, 121, 70, 158, 3, 135, 148, 11, 155,
-        166, 45, 187, 83, 39, 36, 14, 35, 70, 142, 149, 182, 96, 134, 192, 105,
-        42, 16, 118, 137, 107, 50, 5, 186, 101, 96, 18, 166, 168, 200, 28, 178,
-        23, 53, 20, 135, 23, 16, 103};
-      System.out.println("BruteForce Approach:"+s.longestSuccessiveElementsBruteForce(nums));
+       int[] nums = { 5, 8, 3, 2, 1, 4,4};
+    System.out.println("BruteForce Approach:"+s.longestSuccessiveElementsBruteForce(nums));
      System.out.println("Optimal Approach:"+s.longestSuccessiveElementsOptimal(nums));
  
     }
