d01c702c9965fc64ae087b4ad4783dbe57bda2d1
Shresha Acharya <shreshacharya06@gmail.com>
2024-11-26 19:16:40 +0530

    Sub array sum

--- Arrays/Problem30.java
+++ Arrays/Problem30.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 class Solution {
     public int subarraySumBruteForce(int[] nums, int k) {
         int n=nums.length;
@@ -11,6 +12,20 @@ class Solution {
         }
         return count;
     }
+    public int subarraySumOptimal(int[] nums, int k) {
+        int n=nums.length;
+        int count=0;
+        int sum=0;
+        HashMap<Integer,Integer> map= new HashMap<>();
+        map.put(0,1);
+        for(int i=0;i<n;i++){
+            sum=sum+nums[i];
+          int remove=sum-k;
+          count+=map.getOrDefault(remove,0);
+          map.put(sum,map.getOrDefault(sum,0)+1);
+        }
+        return count;
+    }
 }
 public class Problem30 {
     public static void main(String[] args) {
@@ -18,6 +33,7 @@ public class Problem30 {
        int[] nums = {1,2,3};
        int k = 3;
     System.out.println("BruteForce Approach:"+s.subarraySumBruteForce(nums,k));
+    System.out.println("Optimal Approach:"+s.subarraySumOptimal(nums,k));
  
     }
     
