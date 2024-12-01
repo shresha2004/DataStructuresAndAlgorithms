2ccaa9782eccc7ee651a4477c3fc187d42b28414
Shresha Acharya <shreshacharya06@gmail.com>
2024-12-01 19:52:10 +0530

    3 sum problem

--- Arrays/Problem34.java
+++ Arrays/Problem34.java
@@ -2,7 +2,7 @@
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Arrays;
+import java.util.HashSet;
 class Solution {
     public List<List<Integer>> threeSumBruteForce(int[] nums) {
         int n=nums.length;
@@ -25,12 +25,35 @@ class Solution {
         }
         return ans;
     }
+    public List<List<Integer>> threeSumBetter(int[] nums) {
+        int n=nums.length;
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i=0;i<n-1;i++){
+           HashSet<Integer> set = new HashSet<>();
+            for(int j=i+1;j<n;j++){
+               int third= -(nums[i]+nums[j]);
+               if(set.contains(third)){
+                List<Integer> arr= new ArrayList<>();
+                arr.add(nums[i]);
+                arr.add(third);
+                arr.add(nums[j]);
+                Collections.sort(arr);
+               if(!ans.contains(arr)) ans.add(arr);
+               }
+               set.add(nums[j]);
+            }
+            }
+        return ans;
+
+        }
 }
 public class Problem34 {
     public static void main(String[] args) {
         Solution s=new Solution();
        int[] nums = {-1,0,1,2,-1,-4};
-    System.out.println("My Brute Force Approach:"+ s.threeSumBruteForce(nums).toString());
+    System.out.println("Brute Force Approach:"+ s.threeSumBruteForce(nums).toString());
+    System.out.println("Better Approach:"+ s.threeSumBetter(nums).toString());
    
  
     }
