a304030a069a90591aee88657485e5623097bec6
Shresha Acharya <shreshacharya06@gmail.com>
2024-12-12 18:46:33 +0530

    Max len of subbarray with sum 0

--- Arrays/Problem36.java
+++ Arrays/Problem36.java
@@ -1,3 +1,5 @@
+//Problem:https://www.geeksforgeeks.org/problems/largest-subarray-with-0-sum/1?category%5B%5D=Hash&company%5B%5D=Amazon&page=1&query=category%5B%5DHashcompany%5B%5DAmazonpage1company%5B%5DAmazoncategory%5B%5DHash&utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=largest-subarray-with-0-sum
+import java.util.HashMap;
 class Solution {
     int maxLenBruteForce(int arr[]) {
         // code here
@@ -14,13 +16,37 @@ class Solution {
         }
         return maxLength;
     }
+    int maxLenOptimal(int arr[]) {
+        // code here
+        int n=arr.length;
+        HashMap<Integer,Integer> map= new HashMap<>();
+        int maxLength=0;
+        int j=0;
+        int k=0;
+         int sum=0;
+        for(int i=0;i<n;i++){
+                sum=sum+arr[i];
+                if(sum==0) maxLength=i+1;
+                else{
+                    if(map.get(sum)!= null){
+                        maxLength=Math.max(maxLength,i - map.get(sum));
+                    }
+                    else{
+                        map.put(sum,i);
+                    }
+                }
+                
+            
+        }
+        return maxLength;
+    }
 }
 public class Problem36 {
     public static void main(String[] args) {
         Solution s=new Solution();
        int[] nums = {-1,0,1,2,-1,-4};
     System.out.println("Brute Force Approach:"+ s.maxLenBruteForce(nums));
-    // System.out.println("Better Approach:"+ s.threeSumBetter(nums).toString());
+    System.out.println("Better Approach:"+ s.maxLenOptimal(nums));
     // System.out.println("Optimal Approach:"+ s.threeSumBetter(nums).toString());
     }
 }
