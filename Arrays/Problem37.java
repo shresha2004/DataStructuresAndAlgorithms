0952a153258b413ce3a8c684105057242d2ac62e
Shresha Acharya <shreshacharya06@gmail.com>
2024-12-12 22:55:05 +0530

    XOR subarray

--- Arrays/Problem37.java
+++ Arrays/Problem37.java
@@ -1,6 +1,7 @@
  //Problem:https://www.interviewbit.com/problems/subarray-with-given-xor/
  import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
  class Solution {
     public int solveBruteForce(ArrayList<Integer> A, int B) {
         int n= A.size();
@@ -15,11 +16,32 @@ import java.util.Arrays;
         }
         return cnt;
     }
+    public int solveOptimal(ArrayList<Integer> A, int B) {
+        int n= A.size();
+        int cnt=0;
+        int xor=0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(xor,1);
+        for(int i=0;i<n;i++){
+          xor=xor ^ A.get(i);
+          int x=xor ^ B;
+          if(map.containsKey(x)){
+              cnt+=map.get(x);
+          }
+          if(map.containsKey( xor)){
+              map.put(xor,map.get(xor)+1);
+          }
+          else map.put(xor,1);
+            
+        }
+        return cnt;
+    }
 }
 public class Problem37 {
     public static void main(String[] args) {
         Solution s=new Solution();
        ArrayList<Integer> nums =new ArrayList<>(Arrays.asList(4, 2, 2, 6, 4)) ;
     System.out.println("Brute Force Approach:"+ s.solveBruteForce(nums,6));
+    System.out.println("Optimal Approach:"+ s.solveOptimal(nums,6));
     }
 }
