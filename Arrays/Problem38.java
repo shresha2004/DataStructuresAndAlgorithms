a2ee6fc9e6317731a69f76b55daf7995d4f36d05
Shresha Acharya <shreshacharya06@gmail.com>
2024-12-13 19:56:58 +0530

    Merge intervels

--- Arrays/Problem38.java
+++ Arrays/Problem38.java
@@ -32,6 +32,31 @@ class Solution {
       }
       return ans.toArray(new int[ans.size()][]);
     }
+
+
+    public int[][] mergeOptimal(int[][] intervals) {
+        int n = intervals.length;
+        
+      Arrays.sort(intervals,new Comparator<int[]>(){
+        public int compare(int[] a, int[] b){
+            return a[0]-b[0];
+        }
+      });
+      List<int[]> ans = new  ArrayList<>();
+
+      for(int i=0;i<n;i++){
+        
+        if(ans.isEmpty() || intervals[i][0]>ans.get(ans.size()-1)[1]){
+           ans.add(new int[]{intervals[i][0],intervals[i][1]});
+        }
+        
+            else{
+               ans.get(ans.size()-1)[1]=Math.max(ans.get(ans.size()-1)[1],intervals[i][1]);            }
+        }
+        
+      
+      return ans.toArray(new int[ans.size()][]);
+    }
 }
 
 
@@ -46,6 +71,7 @@ public class Problem38 {
             {15, 18}
         };
             System.out.println("Brute Force Approach:"+Arrays.deepToString( s.mergeBruteForce(intervals)));
+            System.out.println("Brute Force Approach:"+Arrays.deepToString( s.mergeOptimal(intervals)));
     }
 }
 
