fd86231b9326a42ef63f8ab8075a568ad7e53e04
Shresha Acharya <shreshacharya06@gmail.com>
2024-12-14 16:33:23 +0530

    Merge sorted arra striver's

--- Arrays/Problem39.java
+++ Arrays/Problem39.java
@@ -30,6 +30,51 @@ class Solution {
        }
        return nums1;
 }
+//Gap method (Shell sort)
+public int[] mergeOptimal(int[] nums1, int m, int[] nums2, int n) {
+    int len = m + n;
+    int gap = (len / 2) + (len % 2);
+
+    while (gap > 0) {
+        int left = 0;
+        int right = left + gap;
+
+        while (right < len) {
+         
+            if (left < m && right < m) {
+                if (nums1[left] > nums1[right]) {
+                    swap(nums1, left, nums1, right);
+                }
+            }
+            else if (left < m && right >= m) {
+                if (nums1[left] > nums2[right - m]) {
+                    swap(nums1, left, nums2, right - m);
+                }
+            }
+            else if (left >= m && right >= m) {
+                if (nums2[left - m] > nums2[right - m]) {
+                    swap(nums2, left - m, nums2, right - m);
+                }
+            }
+            left++;
+            right++;
+        }
+
+        if (gap == 1) break;
+        gap = (gap / 2) + (gap % 2);
+    }
+
+    for (int i = 0; i < n; i++) {
+        nums1[m + i] = nums2[i];
+    }
+    return nums1;
+}
+
+private void swap(int[] arr1, int idx1, int[] arr2, int idx2) {
+    int temp = arr1[idx1];
+    arr1[idx1] = arr2[idx2];
+    arr2[idx2] = temp;
+}
 }
 public class Problem39 {
     public static void main(String[] args) {
@@ -39,6 +84,6 @@ public class Problem39 {
       int[]  nums2 = {2,5,6};
       int n = 3;
             System.out.println("Brute Force Approach:"+Arrays.toString( s.mergeBruteForce(nums1,m,nums2,n)));
-           // System.out.println("Brute Force Approach:"+Arrays.deepToString( s.mergeOptimal()));
+          System.out.println("Brute Force Approach:"+Arrays.toString( s.mergeOptimal(nums1,m,nums2,n)));
     }
 }
