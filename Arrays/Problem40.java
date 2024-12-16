8e22315da120e1f63c96af1bd5f671cac20bd4a8
Shresha Acharya <shreshacharya06@gmail.com>
2024-12-16 19:41:54 +0530

    Find missing and repeated element

--- Arrays/Problem40.java
+++ Arrays/Problem40.java
@@ -26,12 +26,40 @@ class Solution {
         ans.add(missing);
         return ans;
     }
+    ArrayList<Integer> findTwoElementBetter(int arr[]) {
+        // code here
+        ArrayList<Integer> ans= new ArrayList<>();
+       
+        int n=arr.length;
+        int[] freq = new int[n+1];
+        
+        for(int i=0;i<n;i++){
+           
+           freq[arr[i]]=freq[arr[i]]+1;
+           
+        }
+        int missing=-1;
+        int repeating=-1;
+        
+        for(int i=1;i<=n;i++){
+         
+           if(freq[i]==0) missing=i;
+           if(freq[i]==2) repeating=i;
+           
+        }
+        ans.add(repeating);
+        ans.add(missing);
+        
+        
+        return ans;
+    }
 }
 public class Problem40 {
     public static void main(String[] args) {
         Solution s= new Solution();
         int[] arr = {2, 2};
-       System.out.println(s.findTwoElementBruteForce(arr)); 
+       System.out.println("Brute Force Approach:"+s.findTwoElementBruteForce(arr)); 
+       System.out.println("Better Approach:"+s.findTwoElementBetter(arr)); 
 
     }
 }
