4c0b603c55128876f07f829d98509b58dd21f49c
Shresha Acharya <shreshacharya06@gmail.com>
2024-12-20 23:21:33 +0530

    Inversion Count

--- Arrays/Problem41.java
+++ Arrays/Problem41.java
@@ -1,3 +1,4 @@
+//Problem:https://www.geeksforgeeks.org/problems/inversion-of-array-1587115620/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=inversion-of-array
 class Solution {
     // Function to count inversions in the array.
     static int inversionCountBruteForce(int arr[]) {
