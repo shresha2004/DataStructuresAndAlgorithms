e60b706d3b3f0f0bb17ccf1784e3ff963ce90778
Shresha Acharya <shreshacharya06@gmail.com>
2024-12-21 14:40:36 +0530

    Max Product Subarray

--- Arrays/Problem43.java
+++ Arrays/Problem43.java
@@ -16,12 +16,28 @@ class Solution {
         }
         return product;
     }
+    public int maxProductBetter(int[] nums) {
+        int n = nums.length;
+       
+        int product = Integer.MIN_VALUE;
+        
+        for(int i=0;i<n;i++){
+              int checkProduct=1;
+                for(int j=i;j<n;j++){
+                            checkProduct *= nums[j];
+                        product =Math.max(checkProduct,product);
+            }
+            
+        }
+        return product;
+    }
 }
 public class Problem43 {
     public static void main(String[] args) {
         Solution s= new Solution();
         int[] arr = {2,3,-2,4};
        System.out.println("Brute Force Approach:"+s.maxProductBruteForce(arr)); 
+       System.out.println("Better Force Approach:"+s.maxProductBetter(arr)); 
   // System.out.println("Optimal Approach:"+s.reversePairsOptimal(arr)); 
     }
     
