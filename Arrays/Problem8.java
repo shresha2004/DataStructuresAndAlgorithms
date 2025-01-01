2da93f029a92fd232c6bd2e4d4b192ee4389bb54
Shresha Acharya <shreshacharya06@gmail.com>
2025-01-01 22:48:53 +0530

    Missing Number

--- Arrays/Problem8.java
+++ Arrays/Problem8.java
@@ -1,6 +1,6 @@
 //Problem:https://leetcode.com/problems/missing-number/description/
 class Solution {
-    public int missingNumber(int[] nums) {
+    public int missingNumberBruteForce(int[] nums) {
         int n=nums.length;
         int missingNo=nums.length,i,j;
         boolean present;
@@ -22,11 +22,25 @@ class Solution {
         return missingNo;
         
     }
+    public int missingNumberOptimal(int[] nums) {
+        int n=nums.length;
+       
+        int sum1=((n)*(n+1))/2;
+        int sum2=0;
+        for(int i=0;i<n;i++){
+            sum2=sum2+nums[i];
+        }
+        int total=sum1-sum2;
+        
+        return total;
+        
+    }
 }
 public class Problem8 {
     public static void main(String[] args) {
         Solution s =new Solution();
         int[] nums = {9,6,4,2,3,5,7,0,1};
-        System.out.println(s.missingNumber(nums));
+        System.out.println(s.missingNumberBruteForce(nums));
+        System.out.println(s.missingNumberOptimal(nums));
     }
 }
