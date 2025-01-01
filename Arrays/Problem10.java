19a9b147f7f9df194559884dc292995c4e0cad91
Shresha Acharya <shreshacharya06@gmail.com>
2025-01-01 22:28:54 +0530

    Single Number

--- Arrays/Problem10.java
+++ Arrays/Problem10.java
@@ -1,14 +1,14 @@
 //Problem:https://leetcode.com/problems/single-number/description/
 import java.util.HashMap;
 class Solution {
-    public int singleNumber(int[] nums) {
+    public int singleNumberBruteForce(int[] nums) {
         HashMap<Integer,Integer> hash=new HashMap<>();
         int n=nums.length;
         int onlyOne=nums.length;
         for(int num: nums){
             hash.put(num,hash.getOrDefault(num,0)+1);
         }
-        System.out.println(hash);
+        
         for(int i=0;i<n;i++){
             if(hash.get(nums[i])==1){
                 onlyOne=nums[i];
@@ -18,12 +18,22 @@ class Solution {
         
        return onlyOne; 
     }
+    public int singleNumberOptimal(int[] nums) {
+        
+       int result=0;
+       for(int num : nums){
+        result ^= num;
+       }
+        
+       return result; 
+    }
 }
 public class Problem10 {
 public static void main(String[] args) {
     Solution s=new Solution();
         int[] nums = {4,1,2,1,2};
-        System.out.println(s.singleNumber(nums));
+        System.out.println("Brute Force Solution:"+s.singleNumberBruteForce(nums));
+        System.out.println("Optimal Solution:"+s.singleNumberOptimal(nums));
     
 }
 }
