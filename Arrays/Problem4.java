8cb98374eadeca3e772f6833d553bb6a7b1d2a83
Shresha Acharya <shreshacharya06@gmail.com>
2025-01-01 22:38:54 +0530

    Reverse array

--- Arrays/Problem4.java
+++ Arrays/Problem4.java
@@ -1,6 +1,7 @@
+//Problem:https://leetcode.com/problems/rotate-array/description/
 import java.util.Arrays;
 class Solution {
-    public void rotate(int[] nums, int k) {
+    public void rotateBruteForce(int[] nums, int k) {
         int n=nums.length;
         for(int i=0;i<k;i++){
             int temp=nums[n-1];
@@ -13,12 +14,31 @@ class Solution {
         System.out.println(Arrays.toString(nums));
         
     }
+    public void rotateOptimal(int[] nums, int k) {
+        int n=nums.length;
+        k=k%n;
+        reverse(nums,0,n-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,n-1);
+        System.err.println(Arrays.toString(nums));    
+    }
+    public void reverse(int nums[],int start,int end){
+        while(start<end){
+            int temp = nums[start];
+            nums[start]=nums[end];
+            nums[end]=temp;
+            start++;
+            end--;
+        }
+    }
 }
 public class Problem4 {
     public static void main(String[] args) {
         Solution s=new Solution();
-        int[] a={1,2,3,4,5,6,7};
-        s.rotate(a, 3);
+        int[] a1={1,2,3,4,5,6,7};
+        int[] a2={1,2,3,4,5,6,7};
+        s.rotateBruteForce(a1, 3);
+        s.rotateOptimal(a2, 3);
 
     }
     
