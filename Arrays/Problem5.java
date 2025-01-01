a09010efdc84d83e73292a2847217111aeb18d08
Shresha Acharya <shreshacharya06@gmail.com>
2025-01-01 22:45:00 +0530

    Move Zeroes

--- Arrays/Problem5.java
+++ Arrays/Problem5.java
@@ -2,7 +2,7 @@
 import java.util.Arrays;
 
 class Solution {
-    public int[] moveZeroes(int[] nums) {
+    public int[] moveZeroesBruteForce(int[] nums) {
         int n=nums.length;
         
         for(int i=0;i<n;i++){
@@ -21,12 +21,35 @@ class Solution {
         return nums;
         
     }
+    public int[] moveZeroesOptimal(int[] nums) {
+        int n=nums.length;
+        int j=-1;
+         
+        for(int i=0;i<n;i++){
+            if(nums[i]==0){
+                j=i;
+                break;
+                }
+
+            }
+            if(j==-1) return nums;
+        for(int i=j+1;i<n;i++){
+            if(nums[i]!=0){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                j++;
+            }
+        }
+        return nums;
+        }
 }
 public class Problem5 {
     public static void main(String[] args) {
         Solution s= new Solution();
         int[] a={0,1,0,3,12};
-        System.out.println(Arrays.toString(s.moveZeroes(a)));
+        System.out.println(Arrays.toString(s.moveZeroesBruteForce(a)));
+        System.out.println(Arrays.toString(s.moveZeroesOptimal(a)));
     }
     
 }
