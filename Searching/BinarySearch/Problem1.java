ce04658a245a524035117cc47bce5760ea155825
Shresha Acharya <shreshacharya06@gmail.com>
2025-01-17 17:45:29 +0530

    Binary Search

--- Searching/BinarySearch/Problem1.java
+++ Searching/BinarySearch/Problem1.java
@@ -11,7 +11,7 @@ class Solution {
         return -1;
     }
     //Binary Search Algorithm
-    public int searchoptimal(int[] nums, int target) {
+    public int searchOptimal(int[] nums, int target) {
         int n = nums.length;
         int low =0;
         int high = n-1;
@@ -24,15 +24,31 @@ class Solution {
        }
         return -1;
     }
+    //Recurrsive approach of binary search
+    public int searchRecurrsive(int[] nums,int target,int low, int high){
+        if(low>high){
+            return -1;
+        }
+        
+        int mid = (low+high)/2;
+        if(nums[mid] == target) return mid;
+       else if(target > nums[mid]) return searchRecurrsive(nums, target, mid+1, high);
+         return searchRecurrsive(nums, target, low, mid-1);
+        
+        
+    }
 }
 
 public class Problem1 {
     public static void main(String args[]){
         int[] nums={-1,0,3,5,9,12};
-        int target = 9;
+        int target =12;
         Solution s= new Solution();
         System.out.println("BruteForce:"+ s.searchBruteForce(nums, target));
-        System.out.println("Optimal:"+ s.searchoptimal(nums, target));
+        System.out.println("Optimal:"+ s.searchOptimal(nums, target));
+        int n=nums.length;
+        System.out.println("Recurrsive:"+ s.searchRecurrsive(nums, target,0,n-1));
 
     }
 }
+
