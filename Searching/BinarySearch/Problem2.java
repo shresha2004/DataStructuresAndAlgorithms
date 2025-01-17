52bab1174195c0bdbe1f41c20466dd0b6cb40599
Shresha Acharya <shreshacharya06@gmail.com>
2025-01-17 19:40:40 +0530

    Binary Search finding max ele < k

--- Searching/BinarySearch/Problem2.java
+++ Searching/BinarySearch/Problem2.java
@@ -1,7 +1,7 @@
 package BinarySearch;
 //Problem:https://www.geeksforgeeks.org/problems/floor-in-a-sorted-array-1587115620/1?track=DSASP-Searching&amp%253BbatchId=154&utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=floor-in-a-sorted-array
 class Solution{
-    static int findFloorBruteForce(int[] arr, int k) {
+    public int findFloorBruteForce(int[] arr, int k) {
         
         int n=arr.length;
         int largeEle = Integer.MIN_VALUE;
@@ -14,6 +14,39 @@ class Solution{
        }
        return index;
     }
+
+    public int findFloorMyOptimal(int[] arr, int k) {
+        // write code here
+        int n = arr.length;
+        int low =0;
+        int high = n-1;
+        int largeEle = Integer.MIN_VALUE;
+        int index = -1 ;
+        while(low <= high){
+            int mid = (low+high)/2;
+            if(k<arr[mid] ) {
+                high=mid-1;
+                if(arr[mid] > largeEle && arr[mid] < k){
+                    largeEle = arr[mid];
+                    index = mid;
+                }
+               
+            }
+          else  if(k>arr[mid]){
+                low=mid+1;
+                 if(arr[mid] > largeEle && arr[mid] < k){
+                    largeEle = arr[mid];
+                    index = mid;
+                }
+            }
+            else return mid;
+            
+             
+        }
+        
+       return index;
+    }
+
 }
 
 public class Problem2 {
@@ -21,6 +54,7 @@ public class Problem2 {
         Solution s = new Solution();
         int[] arr={1, 2, 8, 10, 11, 12, 19};
         System.out.println("BruteForce:"+s.findFloorBruteForce(arr, 5));
+        System.out.println("My Optimal:"+s.findFloorMyOptimal(arr, 5));
         
     }
 }
