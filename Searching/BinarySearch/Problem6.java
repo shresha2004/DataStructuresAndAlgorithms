fe87d8602ee2060e9a82af4a0d69e42d2d53b8a7
Shresha Acharya <shreshacharya06@gmail.com>
2025-01-20 10:42:22 +0530

    Lower bound and Upper Bound (Binary Search

--- Searching/BinarySearch/Problem6.java
+++ Searching/BinarySearch/Problem6.java
@@ -2,7 +2,7 @@ package BinarySearch;
 //Problem:https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
 import java.util.Arrays;
 class Solution {
-    public int[] searchRangeBruteForce(int[] nums, int target) {
+    public int[] searchRangeMyBruteForce(int[] nums, int target) {
         int n = nums.length;
         int flag =0;
         int start = -1;
@@ -29,13 +29,63 @@ class Solution {
         return ans;
         
     }
+
+    //BinarySearch
+    public int[] searchRangeOptimal(int[] nums, int target) {
+        int n = nums.length;
+       int start = lowerBound(nums, target, n);
+       if(start == -1) return new int[] {-1,-1};
+       int end = upperBound(nums,target, n);
+   
+       return new int[] {start,end}; 
+    }
+
+
+    private int lowerBound(int[] nums, int target, int n){
+       int low = 0;
+       int high = n-1;
+       int start = -1;
+       while(low<= high){
+        int mid = (low+high)/2;
+        if(nums[mid] == target){
+            start = mid;
+            high = mid-1;
+        }
+        else if (target < nums[mid]){
+            high = mid -1;
+
+        }
+        else low = mid +1;
+       }
+
+       return start;
+    }
+
+    private int upperBound(int[] nums, int target, int n){
+        int low =0;
+        int high = n-1;
+        int end = -1;
+
+        while(low <= high){
+            int mid = (low+high)/2;
+            if(target == nums[mid]){
+                end = mid;
+                low = mid+1;
+            }
+            else if ( target < nums[mid]) high = mid-1;
+            else low = mid+1;
+
+        }
+        return end;
+    }
 }
 public class Problem6 {
     public static void main(String[] args) {
         Solution s = new Solution();
         int[] nums = {5,7,7,8,8,10};
         int target = 8;
-        System.out.println("My Bruteforce:"+ Arrays.toString(s.searchRangeBruteForce(nums, target)));
+        System.out.println("My Bruteforce:"+ Arrays.toString(s.searchRangeMyBruteForce(nums, target)));
+        System.out.println("Optimal:"+ Arrays.toString(s.searchRangeOptimal(nums, target)));
     }
     
 }
