f79eb1c19be6c2617de9cbddd3072ea752798847
Shresha Acharya <shreshacharya06@gmail.com>
2025-01-21 19:10:41 +0530

    Rotated Sorted Array Problem

--- Searching/BinarySearch/Problem8.java
+++ Searching/BinarySearch/Problem8.java
@@ -11,6 +11,27 @@ class Solution {
         }
         return -1;
     }
+
+    
+    public int searchOptimal(int[] nums, int target) {
+        int n = nums.length;
+       int low = 0;
+       int high = n-1;
+        while(low <= high){
+            int mid = (low+high)/2;
+            if(target == nums[mid]) return mid;
+            else if(nums[low] <= nums[mid]){
+                if(target <= nums[mid] && target >= nums[low]){
+                    high = mid-1;
+                }else low = mid+1;
+            }
+            else {
+                if(target >= nums[mid] && target <= nums[high]) low = mid + 1;
+                else high = mid - 1;
+            };
+        }
+        return -1;
+    }
 }
 public class Problem8 {
     public static void main(String[] args) {
@@ -18,6 +39,7 @@ public class Problem8 {
       int[]  nums = {4,5,6,7,0,1,2};
       int target = 0;
       System.out.println("My Brute Force:"+s.searchBruteForce(nums, target));
+      System.out.println("Optimal:"+s.searchOptimal(nums, target));
     }
     
 }
