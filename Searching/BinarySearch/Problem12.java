d76b5acb3a3bbbbdc2ca5b54eb38a0c0581f8f13
Shresha Acharya <shreshacharya06@gmail.com>
2025-01-23 22:15:18 +0530

    Single Non Duplicate using binary search

--- Searching/BinarySearch/Problem12.java
+++ Searching/BinarySearch/Problem12.java
@@ -18,6 +18,26 @@ class Solution {
       }
       return 0;
     }
+
+    public int singleNonDuplicateOptimal(int[] nums) {
+        int n = nums.length;
+      if (n==1) return nums[0];
+      else if(nums[0] != nums[1]) return nums[0];
+      else if( nums[n-2] != nums[n-1]) return nums[n-1];
+      int low = 1;
+      int high = n-2;
+      while(low<=high){
+        int mid = (low+high)/2;
+        if(nums[mid] != nums[mid+1] && nums[mid] != nums[mid-1]) return nums[mid];
+        if( (mid%2 == 0 && nums[mid]== nums[mid+1]) || (mid%2 == 1 && nums[mid] == nums[mid-1])){
+            low = mid+1;
+        }
+        else {
+            high = mid-1;
+        }
+      }
+      return 0;
+    }
 }
    
 
@@ -26,5 +46,6 @@ public class Problem12 {
         Solution s = new Solution();
         int [] nums = {1,1,2,3,3,4,4,8,8};
         System.out.println("Brute Force:"+s.singleNonDuplicateBruteForce(nums));
+        System.out.println("Optimal:"+s.singleNonDuplicateOptimal(nums));
     }
 }
