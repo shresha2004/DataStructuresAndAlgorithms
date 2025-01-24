dd049dfa916d3499bbb608079f7cee0439b9ad35
Shresha Acharya <shreshacharya06@gmail.com>
2025-01-24 19:44:11 +0530

    Find Peak Element

--- Searching/BinarySearch/Problem13.java
+++ Searching/BinarySearch/Problem13.java
@@ -4,22 +4,34 @@ package BinarySearch;
 class Solution {
     public int findPeakElementBruteForce(int[] nums) {
         int n = nums.length;
-        int max = nums[0];
-        int index = 0;
-        for(int i=1; i<n;i++){
-            if(nums[i]> max){
-                max = nums[i];
-                index = i;
-            }
+        for(int i=0; i<n; i++){
+            if((i==0 || nums[i]> nums[i-1]) && (i==n-1 || nums[i]> nums[i+1])) return i;
         }
-       return index; 
+      
+        return -1;
+    }
+
+    public int findPeakElementOptimal(int[] nums) {
+        int n = nums.length;
+        int low =0;
+        int high = n-1;
+        
+        while(low <= high){
+            int mid = (low+high)/2;
+            if((mid == 0 || nums[mid]> nums[mid-1]) && (mid == n-1 || nums[mid]> nums[mid+1])) return mid;
+            else if(nums[mid] < nums[mid+1]) low = mid+1;
+            else high = mid -1;
+        }
+      
+        return -1;
     }
 }
 
 public class Problem13 {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int [] nums = {1,2,1,3,5,6,4};
+        int [] nums = {1,2,3,1};
         System.out.println("Brute Force:"+s.findPeakElementBruteForce(nums));
+        System.out.println("Optimal:"+s.findPeakElementOptimal(nums));
     }
 }
