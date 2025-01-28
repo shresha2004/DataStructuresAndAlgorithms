bcccafa9ecc62e43e7a21f8a3bb75c6dae1e75af
Shresha Acharya <shreshacharya06@gmail.com>
2025-01-28 19:03:49 +0530

    Intersections of two array

--- Arrays/Problem44.java
+++ Arrays/Problem44.java
@@ -7,7 +7,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 class Solution {
-    public int[] intersection(int[] nums1, int[] nums2) {
+    public int[] intersectionBruteForce(int[] nums1, int[] nums2) {
         int n1 = nums1.length;
         int n2 = nums2.length;
         Set<Integer> s = new HashSet<>();
@@ -28,6 +28,26 @@ class Solution {
         }
         return ans;
     }
+    public int[] intersectionBetter(int[] nums1, int[] nums2) {
+        
+       HashSet<Integer> arr = new HashSet<>();
+       HashSet<Integer> ans = new HashSet<>();
+
+       for(int num : nums1){
+        arr.add(num);
+       }
+       for(int num : nums2){
+        if(arr.contains(num)){
+            ans.add(num);
+        }
+       }
+        int[] ansArr = new int[ans.size()];
+        int i=0;
+        for(int num : ans){
+            ansArr[i++] = num;
+        }
+       return ansArr;
+    }
 }
 
 public class Problem44 {
@@ -35,6 +55,7 @@ public class Problem44 {
         Solution s = new Solution();
         int [] nums1 = {1,2,2,1};
         int[]  nums2 = {2,2};
-        System.out.println(Arrays.toString(s.intersection(nums1, nums2)));
+        System.out.println("Brute Force:"+Arrays.toString(s.intersectionBruteForce(nums1, nums2)));
+        System.out.println("Better:"+Arrays.toString(s.intersectionBetter(nums1, nums2)));
     }
 }
