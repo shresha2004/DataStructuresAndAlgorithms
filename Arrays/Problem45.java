93d9872378b63a6801903a61f6e52b5b72832d73
Shresha Acharya <shreshacharya06@gmail.com>
2025-02-01 23:37:21 +0530

    Sorted square array

--- Arrays/Problem45.java
+++ Arrays/Problem45.java
@@ -3,7 +3,7 @@
 import java.util.Arrays;
 
 class Solution {
-    public int[] sortedSquaresBruteForce(int[] nums) {
+    public int[] sortedSquaresMyBruteForce(int[] nums) {
         int n = nums.length;
         int[] sqrArray = new int[n];
         for(int i =0; i<n;i++){
@@ -25,12 +25,40 @@ class Solution {
         }
         return sqrArray;
     }
+
+    public int[] sortedSquaresMyOptimal(int[] nums) {
+        int n = nums.length;
+        int[] sqrArray = new int[n];
+        //Two pointer approach
+        int low = 0;
+        int high = n-1;
+        int i = n-1;
+        while(low <= high){
+             int mkPos = nums[low];
+            if(nums[low]<0){
+                 mkPos = mkPos * -1;
+            }
+                if(mkPos < nums[high]){
+                    sqrArray[i] = nums[high] * nums[high];
+                    i--;
+                    high--;
+                }
+                else{
+                 sqrArray[i] = nums[low] * nums[low];
+                 i--;
+                    low++;
+                }
+            
+        }
+        return sqrArray;
+    }
 }
 public class Problem45 {
      public static void main(String[] args) {
         Solution s = new Solution();
         int [] nums = {-4,-1,0,3,10};
-       
-        System.out.println("Brute Force:"+Arrays.toString(s.sortedSquaresBruteForce(nums)));
+
+        System.out.println("My Brute Force:"+Arrays.toString(s.sortedSquaresMyBruteForce(nums)));
+        System.out.println("My Optimal:"+Arrays.toString(s.sortedSquaresMyOptimal(nums)));
     }
 }
