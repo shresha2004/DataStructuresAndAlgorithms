1f398bed6e2fe477da8063216d9f262aa62b7ec4
Shresha Acharya <shreshacharya06@gmail.com>
2025-02-08 21:58:21 +0530

    Finding Disappeared Number

--- Arrays/Problem49.java
+++ Arrays/Problem49.java
@@ -13,11 +13,29 @@ class Solution {
         }
         return ans;
     }
+
+    //Index matching approach
+    public List<Integer> findDisappearedNumbersOptimal(int[] nums) {
+        int n =  nums.length;
+        List<Integer> ans = new ArrayList<>();
+       
+       for(int num :nums){
+        int index = Math.abs(num)-1;
+       if(nums[index] > 0) nums[index] = - nums[index];
+       }
+       for(int i = 0;i<n;i++){
+        if(nums[i]>0) ans.add(i+1);
+       }
+       
+        return ans;
+    }
+   
 }
 public class Problem49 {
     public static void main(String[] args) {
         Solution s = new Solution();
         int[] nums = {4,3,2,7,8,2,3,1};
         System.out.println("Brute Force:"+s.findDisappearedNumbersBruteForce(nums));
+        System.out.println("Optimal:"+s.findDisappearedNumbersOptimal(nums));
     }
 }
