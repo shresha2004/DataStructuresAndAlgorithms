24efd4fde5eab49f976421815e72218e6bc46468
Shresha Acharya <shreshacharya06@gmail.com>
2025-02-09 19:03:24 +0530

    In place marking approach

--- Arrays/Problem50.java
+++ Arrays/Problem50.java
@@ -1,5 +1,18 @@
 //Problem:https://leetcode.com/problems/find-the-duplicate-number/description/
+
+import java.util.HashMap;
 class Solution {
+    public int findDuplicateBruteForce(int[] nums) {
+        int n= nums.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<n;i++){
+          map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+            if(map.get(nums[i]) > 1) return nums[i];
+                    }
+
+        return 0;
+    }
+    //Inplace marking approach
     public int findDuplicateOptimal(int[] nums) {
         int n= nums.length;
         
@@ -15,6 +28,7 @@ public class Problem50 {
     public static void main(String[] args) {
         Solution s = new Solution();
         int[] nums = {1,3,4,2,2};
+        System.out.println("Brute Force:"+s.findDuplicateBruteForce(nums));
         System.out.println("Optimal:"+s.findDuplicateOptimal(nums));
     }
 }
