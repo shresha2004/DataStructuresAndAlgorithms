121f999f7069ad406b252709033e567321a6e30a
Shresha Acharya <shreshacharya06@gmail.com>
2025-02-21 21:44:34 +0530

    Valid parenthisis

--- Arrays/Problem55.java
+++ Arrays/Problem55.java
@@ -1,7 +1,8 @@
+//Problem:https://leetcode.com/problems/valid-parentheses/description/
+import java.util.Stack;
 class Solution {
     public boolean isValidBruteForce(String s) {
-        int n = s.length();
-        int count =0;
+        
        while(s.contains("()") || s.contains("{}") || s.contains("[]") ){
             s = s.replace("()","")
                   .replace("{}","")
@@ -12,10 +13,27 @@ class Solution {
        return false;
         
     }
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char ch : s.toCharArray()){
+            if(ch == '(' || ch=='{'||ch=='[') stack.push(ch);
+            else{
+                if(stack.isEmpty()) return false;
+                char top = stack.pop();
+                if((ch ==')' && top != '(') || 
+                (ch == '}' && top != '{' ) || 
+                (ch == ']' && top != '[') ) 
+                return false;
+            }
+        }
+        return stack.isEmpty();
+        
+    }
 }
 public class Problem55 {
     public static void main(String[] args) {
         Solution s = new Solution();
         System.out.println("Brute Force:"+s.isValidBruteForce("(){}[]"));
+        System.out.println("Optimal:"+s.isValidBruteForce("(){}[]"));
     }
 }
