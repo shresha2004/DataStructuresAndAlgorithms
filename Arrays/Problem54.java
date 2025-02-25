26248793a312d0fce67c8a2ef1e8fad7541d9235
Shresha Acharya <shreshacharya06@gmail.com>
2025-02-25 14:02:48 +0530

    In-place marking approach (Cyclic sorting)

--- Arrays/Problem54.java
+++ Arrays/Problem54.java
@@ -15,12 +15,26 @@ class Solution {
         }
         return 0;
     }
+    public int firstMissingPositiveOptimal(int[] nums) {
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+           while(nums[i]>0 && nums[i]<=n && nums[i] !=nums[nums[i]-1] ){
+            int temp = nums[i];
+            nums[i] = nums[temp-1];
+            nums[temp-1]=temp;
+           }
+        }
+        for(int i = 0;i<n;i++){
+            if(nums[i]!=i+1) return i+1;
+        }
+        return n+1;
+    }
 }
 public class Problem54 {
     public static void main(String[] args) {
         Solution s = new Solution();
         int[] nums = {3,4,-1,1};
         System.out.println("My Brute Force:"+(s.firstMissingPositiveBruteForce(nums)));
-        System.out.println("My Brute Force:"+(s.firstMissingPositiveBruteForce(nums)));
+        System.out.println("Optimal:"+(s.firstMissingPositiveBruteForce(nums)));
     }
 }
