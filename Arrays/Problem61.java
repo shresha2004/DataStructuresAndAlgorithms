c7036d64ba6e3d56ecafab39587e4707a8ef3f2c
Shresha Acharya <shreshacharya06@gmail.com>
2025-03-06 00:08:10 +0530

    Trapped water

--- Arrays/Problem61.java
+++ Arrays/Problem61.java
@@ -16,11 +16,37 @@ class Solution {
         }
         return units;
     }
+
+    public int trapOptimal(int[] height) {
+        int left = 0, right = height.length-1;
+        int leftMax = 0, rightMax = 0;
+        int trappedWater =0;
+        while(left < right){
+         if(height[left]<height[right]){
+             if(height[left]>= leftMax){
+                 leftMax =height[left];
+             }else{
+                 trappedWater += leftMax - height[left];
+             }
+             left++;
+         }
+         else{
+             if(height[right]>= rightMax){
+                 rightMax = height[right];
+             }else{
+                 trappedWater += rightMax - height[right];
+             }
+             right--;
+         }
+        }
+        return trappedWater;
+     }
 }
 public class Problem61 {
     public static void main(String[] args) {
         Solution s = new Solution();
         int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
         System.out.println("Brute Force:"+s.trapBruteForce(height));
+        System.out.println("Optimal:"+s.trapOptimal(height));
     }
 }
