6e774407903a666eea10ba2bf24f37a79a059be5
Shresha Acharya <shreshacharya06@gmail.com>
2025-03-06 16:19:45 +0530

    Trapped water

--- Arrays/Problem62.java
+++ Arrays/Problem62.java
@@ -1,6 +1,17 @@
 //Problem:https://leetcode.com/problems/container-with-most-water/description/
 //I choosed question
 class Solution {
+    public int maxAreaBruteForce(int[] height) {
+        int n = height.length;
+        int maxWater = 0;
+        for(int i =0;i<n-1;i++){
+            for(int j = i+1;j<n;j++){
+                int temp = (j-i) * Math.min(height[i],height[j]);
+                maxWater = Math.max(maxWater,temp);
+            }
+        }
+        return maxWater;
+    }
     public int maxAreaOptimal(int[] height) {
         int n = height.length;
         int left = 0,right = n-1;
@@ -26,7 +37,8 @@ public class Problem62 {
     public static void main(String[] args){
         Solution s = new Solution();
         int[] height = {1,8,6,2,5,4,8,3,7};
+        
+        System.out.println("MyBruteForce:"+s.maxAreaBruteForce(height));
         System.out.println("My Optimal:"+s.maxAreaOptimal(height));
-
     }
 }
