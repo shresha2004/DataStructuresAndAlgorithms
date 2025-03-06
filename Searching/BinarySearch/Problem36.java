6ff58a12cecc4a97201d5d47f902905ce081be8d
Shresha Acharya <shreshacharya06@gmail.com>
2025-03-06 23:24:16 +0530

    Median of 2 sorted arrays

--- Searching/BinarySearch/Problem36.java
+++ Searching/BinarySearch/Problem36.java
@@ -79,6 +79,34 @@ class Solution {
             return (float) (ele1+ele2)/2;
 
         }
+
+
+        public double findMedianSortedArraysOptimal(int[] nums1, int[] nums2) {
+            int n1 = nums1.length;
+            int n2 = nums2.length;
+            int n = n1+n2;
+            if(n1>n2) return findMedianSortedArraysOptimal(nums2,nums1);
+            int low = 0;
+            int high = n1;
+            int left = (n1+n2+1)/2;
+            while(low <= high){
+                int mid1 = (low+high)/2;
+                int mid2 = left - mid1;
+    
+                int l1 = (mid1>0) ? nums1[mid1-1] : Integer.MIN_VALUE;
+                int l2 = (mid2>0) ? nums2[mid2-1] : Integer.MIN_VALUE;
+                int r1 = (mid1<n1) ? nums1[mid1] : Integer.MAX_VALUE;
+                int r2 = (mid2<n2) ? nums2[mid2] : Integer.MAX_VALUE;
+    
+                if(l1<=r2 && l2 <=r1){
+                    if(n%2 !=0) return Math.max(l1,l2);
+                    else return ((double) (Math.max(l1,l2)+Math.min(r1,r2))/2);
+                }
+                else if(l1 > r2) high = mid1-1;
+                else low = mid1+1;
+            }
+            return 0;
+            }
 }
 
 public class Problem36 {
@@ -86,7 +114,8 @@ public class Problem36 {
         int[] nums1 = {1,3}, nums2 = {2};
         Solution s = new Solution();
         System.out.println("My Brute Force:"+s.findMedianSortedArraysBruteForce(nums1, nums2));
-        System.out.println("Optimal:"+s.findMedianSortedArraysBetter(nums1, nums2));
+        System.out.println("Better:"+s.findMedianSortedArraysBetter(nums1, nums2));
+        System.out.println("Optimal:"+s.findMedianSortedArraysOptimal(nums1, nums2));
 
     }
     
