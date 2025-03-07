8e98234d3948f4d23ad1e9cafee537a24c803c6d
Shresha Acharya <shreshacharya06@gmail.com>
2025-03-07 10:41:39 +0530

    Kth element from 2 Sorted array

--- Searching/BinarySearch/Problem37.java
+++ Searching/BinarySearch/Problem37.java
@@ -30,6 +30,44 @@ class Solution {
         }
         return merged[k-1];
     }
+
+    public int kthElementBetter(int a[], int b[], int k) {
+        // code here
+        int n1 = a.length;
+        int n2 = b.length;
+        int count =0;
+        
+        int left = 0;
+        int right = 0;
+        while(left < n1 && right < n2){
+            if(a[left] < b[right]){
+                count++;
+                if(count == k) return a[left];
+                left++;
+
+            }
+            else {
+                
+                count++;
+                if(count == k) return b[right];
+                right++;
+            }
+        }
+        
+        while(left < n1){
+            
+            count++;
+            if(count == k) return a[left];
+            left++;
+        }
+        while(right < n2){
+            
+            count++;
+            if(count == k) return b[right];
+            right++;
+            }
+        return 0;
+    }
 }
 
 public class Problem37 {
@@ -38,5 +76,6 @@ public class Problem37 {
         int[] a = {2, 3, 6, 7, 9}, b = {1, 4, 8, 10};
         int k = 5;
         System.out.println("Brute Force:"+s.kthElementBruteForce(a,b,k));
+        System.out.println("Better:"+s.kthElementBetter(a,b,k));
     }
 }
