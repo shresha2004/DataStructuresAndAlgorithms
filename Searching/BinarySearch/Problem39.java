958725562ab7e3241b271e0328094b9c86d5a5ca
Shresha Acharya <shreshacharya06@gmail.com>
2025-03-07 17:35:51 +0530

    Find the target ele in sorted matrix

--- Searching/BinarySearch/Problem39.java
+++ Searching/BinarySearch/Problem39.java
@@ -3,6 +3,7 @@ package BinarySearch;
 class Solution {
     public boolean searchMatrixBruteForce(int[][] matrix, int target) {
         int m = matrix.length;
+    
         int n = matrix[0].length;
        for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
