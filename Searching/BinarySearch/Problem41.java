fd853e682ab68c7d65ceaafabb26b50e4fa7bae7
Shresha Acharya <shreshacharya06@gmail.com>
2025-03-08 11:26:21 +0530

    Find peak ele in grid

--- Searching/BinarySearch/Problem41.java
+++ Searching/BinarySearch/Problem41.java
@@ -2,7 +2,7 @@
 package BinarySearch;
 import java.util.Arrays;
 class Solution {
-    public int[] findPeakGrid(int[][] mat) {
+    public int[] findPeakGridBruteForce(int[][] mat) {
         int m = mat.length;
         int n = mat[0].length;
         int[] ans = new int[2];
@@ -21,11 +21,43 @@ class Solution {
 
         return ans;
     }
+
+    public int[] findPeakGridOptimal(int[][] mat) {
+        int n = mat[0].length;
+        int  low = 0;
+        int high = n-1;
+         while(low <= high){
+            int mid = (low+high) >> 1;
+            int row = findRow(mat,mid);
+            int left = (mid-1>=0)? mat[row][mid-1] : -1;
+            int right = (mid+1<n)?mat[row][mid+1] : -1;
+
+            if(mat[row][mid]> left && mat[row][mid]>right) return new int[] {row,mid};
+            else if(left>mat[row][mid]) high = mid-1;
+            else low = mid+1;
+         }
+
+      return new int[] {-1,-1};
+    }
+
+    private int findRow(int[][] mat,int mid){
+        int n = mat.length;
+        int maxEle =-1;
+        int index = -1;
+        for(int i=0;i<n;i++){
+            if(mat[i][mid]>maxEle){
+                maxEle = mat[i][mid];
+                index = i;
+            }
+        }
+        return index;
+    }
 }
 public class Problem41 {
     public static void main(String[] args) {
         Solution s = new Solution();
         int[][] mat = {{1,4},{3,2}};
-        System.out.println("Brute Force:"+Arrays.toString(s.findPeakGrid(mat)));
+        System.out.println("Brute Force:"+Arrays.toString(s.findPeakGridBruteForce(mat)));
+        System.out.println("Optimal:"+Arrays.toString(s.findPeakGridOptimal(mat)));
     }
 }
