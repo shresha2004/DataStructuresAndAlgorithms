3c7575522cebd0dd19e588f2035900225aa65249
Shresha Acharya <shreshacharya06@gmail.com>
2025-03-10 19:48:41 +0530

    Isomerphic string

--- Strings/Problem5.java
+++ Strings/Problem5.java
@@ -19,11 +19,33 @@ class Solution {
        }
        return true;
     }
+
+    public boolean isIsomorphicOptimal(String s, String t){
+        HashMap<Character,Character> map= new HashMap<>();
+        HashMap<Character,Character> reverseMap = new HashMap<>();
+        int sLen = s.length();
+ 
+        for(int i=0;i<sLen;i++){
+        char key = s.charAt(i);
+         char value = t.charAt(i);
+         if(map.containsKey(key)){
+             if(map.get(key)!= value) return false;
+         }
+         else {
+             if(reverseMap.containsKey(value)) return false;
+             map.put(key,value);
+             reverseMap.put(value,key);
+         }
+        
+        }
+        return true;
+     }
 }
 public class Problem5 {
     public static void main(String[] args) {
         Solution s = new Solution();
         String st = "egg", t = "add";
         System.out.println("Brute Force:"+s.isIsomorphicBruteForce(st, t));
+        System.out.println("Optimal:"+s.isIsomorphicOptimal(st, t));
     }
 }
