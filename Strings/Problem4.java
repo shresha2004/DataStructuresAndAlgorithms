81e2d6ffcd91be49fcdcfd5d500238fca575aefc
Shresha Acharya <shreshacharya06@gmail.com>
2025-03-10 18:25:02 +0530

    Longest common prefix

--- Strings/Problem4.java
+++ Strings/Problem4.java
@@ -13,11 +13,27 @@ class Solution {
         }
         return prefix;
     }
+
+    public String longestCommonPrefixOptimal(String[] strs) {
+        int n  = strs.length;
+        if(n==0) return "";
+        for(int i=0;i<strs[0].length();i++){
+         char check = strs[0].charAt(i);
+         for(int j=1;j<strs.length;j++){
+             if(i>=strs[j].length() || strs[j].charAt(i) != check ){
+                 return strs[j].substring(0,i);
+             }
+         }
+ 
+        }
+        return strs[0];
+     }
 }
 public class Problem4 {
     public static void main(String[] args) {
         Solution s = new Solution();
         String[] strs = {"flower","flow","flight"};
         System.out.println("Brute Force:"+s.longestCommonPrefixBruteForce(strs));
+        System.out.println("Optimal:"+s.longestCommonPrefixOptimal(strs));
     }
 }
