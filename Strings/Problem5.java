deb50e5c6f8ef95890f2a51d8e8fbe1280f7bc5f
Shresha Acharya <shreshacharya06@gmail.com>
2025-03-10 19:33:07 +0530

    Isomerphic string

---
+++ Strings/Problem5.java
@@ -0,0 +1,29 @@
+//Problem:https://leetcode.com/problems/isomorphic-strings/description/
+import java.util.HashMap;
+class Solution {
+    public boolean isIsomorphicBruteForce(String s, String t){
+       HashMap<Character,Character> map = new HashMap<>();
+       int sLen = s.length();
+
+       for(int i=0;i<sLen;i++){
+       char key = s.charAt(i);
+        char value = t.charAt(i);
+        if(map.containsKey(key)){
+            if(map.get(key)!= value) return false;
+        }
+        else {
+            if(map.containsValue(value)) return false;
+            map.put(key,value);
+        }
+       
+       }
+       return true;
+    }
+}
+public class Problem5 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String st = "egg", t = "add";
+        System.out.println("Brute Force:"+s.isIsomorphicBruteForce(st, t));
+    }
+}
