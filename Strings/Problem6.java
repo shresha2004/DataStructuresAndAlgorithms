24439c68285019d90518d375269a85002a971406
Shresha Acharya <shreshacharya06@gmail.com>
2025-03-11 17:42:43 +0530

    Rotated string

---
+++ Strings/Problem6.java
@@ -0,0 +1,20 @@
+//Problem:https://leetcode.com/problems/rotate-string/description/
+class Solution {
+    public boolean rotateStringBruteForce(String s, String goal) {
+        if(s.length() != goal.length()) return false;
+        String doubledString= s+s;
+        System.out.println(doubledString);
+        int length= goal.length();
+        for(int i=0;i<length;i++){
+            if(doubledString.substring(i,i+length).equals(goal)) return true;
+        }
+       return false; 
+    }
+}
+public class Problem6 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        String ss = "abcde", goal = "cdeab";
+        System.out.println("Brute Force:"+s.rotateStringBruteForce(ss, goal));
+    }
+}
