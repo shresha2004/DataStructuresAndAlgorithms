5e0a0c384174cb479c596431a976793439fc8877
Shresha Acharya <shreshacharya06@gmail.com>
2025-03-12 18:26:33 +0530

    Is anagram

--- Strings/Problem7.java
+++ Strings/Problem7.java
@@ -1,12 +1,13 @@
 //Problem:https://leetcode.com/problems/valid-anagram/description/
 import java.util.Arrays;
+import java.util.ArrayList;
 class Solution {
     public boolean isAnagramBruteForce(String s, String t) {
         if(s.length()!= t.length()) return false;
          char[] charArrS = s.toCharArray();
          char[] charArrT = t.toCharArray();
-         Arrays.sort(charArrS);
-         Arrays.sort(charArrT);
+         mergeSort(charArrS,0,charArrS.length-1);
+         mergeSort(charArrT,0,charArrT.length-1);
          return Arrays.equals(charArrS,charArrT);
     }
 
@@ -22,8 +23,39 @@ class Solution {
          }
          return true;
     }
+    
+    private void mergeSort(char[] arr,int low,int high){
+        if(high<=low) return;
+        int mid = (low+high)/2;
+        mergeSort(arr,low,mid);
+        mergeSort(arr,mid+1,high);
+        merge(arr,low,mid,high);
+    }
+    private void merge(char[] arr,int low,int mid,int high){
+        int left = low;
+        int right = mid+1;
+        ArrayList<Character> temp= new ArrayList<>();
+        while(left <= mid && right<=high){
+            if(arr[left]<arr[right]){
+                temp.add(arr[left]);
+                left++;
+            }else{
+                temp.add(arr[right]);
+                right++;
+            }
+        }
+        while(left <= mid){
+            temp.add(arr[left++]);
+        }
+        while(right <= high){
+            temp.add(arr[right++]);
+        }
+        int j=0;
+        for(int i=low;i<=high;i++){
+            arr[i]=temp.get(j++);
+        }
 }
-
+}
 
 public class Problem7 {
     public static void main(String[] args) {
