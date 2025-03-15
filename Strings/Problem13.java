a23053a6307d560dd833188c1f1e868854bd43c6
Shresha Acharya <shreshacharya06@gmail.com>
2025-03-15 19:48:39 +0530

    Beauty sum

--- Strings/Problem13.java
+++ Strings/Problem13.java
@@ -23,11 +23,36 @@ class Solution {
         }
         return sum;
     }
+    public int beautySumOptimal(String s) {
+        int n = s.length();
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+           int[] freq = new int[26];
+            for (int j = i; j < n; j++) {
+                freq[s.charAt(j)-'a']++;
+                
+                int max = 0;
+                int min = Integer.MAX_VALUE;
+                
+                for (int k=0;k<26;k++) {
+                    if(freq[k]>0){
+                    max = Math.max(max, freq[k]);
+                    min = Math.min(min, freq[k]);
+                    }
+                }
+               sum += (max - min); 
+            }
+                            
+
+        }
+        return sum;
+    }
 }
 public class Problem13 {
     public static void main(String[] args) {
         Solution s = new Solution();
         String ss = "aabcb";
         System.out.println("Brute Force:"+s.beautySumBruteForce(ss));
+        System.out.println("Optimal:"+s.beautySumOptimal(ss));
     }
 }
