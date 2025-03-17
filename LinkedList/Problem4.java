7231654110caaa7d847eb70c53f31760328c122f
Shresha Acharya <shreshacharya06@gmail.com>
2025-03-17 19:56:48 +0530

    Length of the LL

--- LinkedList/Problem4.java
+++ LinkedList/Problem4.java
@@ -1,4 +1,4 @@
-//Problem:
+//Problem:https://www.geeksforgeeks.org/problems/search-in-linked-list-1664434326/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=search-in-linked-list-1664434326
 class Node{
     int data;
     Node next;
