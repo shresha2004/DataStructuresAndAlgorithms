d3244616b3a75a69d4c1cf4adde4a339e70d9f80
Shresha Acharya <shreshacharya06@gmail.com>
2025-03-22 21:52:32 +0530

    Longest unique char substring

--- Strings/Problem16.java
+++ Strings/Problem16.java
@@ -1,7 +1,7 @@
 //Problem:https://leetcode.com/problems/longest-substring-without-repeating-characters/description/?envType=problem-list-v2&envId=string
-
+import java.util.HashSet;
 class Solution {
-    public int lengthOfLongestSubstring(String s) {
+    public int lengthOfLongestSubstringBruteForce(String s) {
         if (s.equals("") || s == null)
             return 0;
         int n = s.length();
@@ -31,11 +31,32 @@ class Solution {
 
         return maxLen;
     }
+    public int lengthOfLongestSubstringOptimal(String s) {
+        if (s.equals("") || s == null)
+            return 0;
+        HashSet<Character> set= new HashSet<>();
+       int maxLen = 0;
+       int n = s.length();
+       int left =0;
+       int right = 0;
+       while(right < n){
+        if(!set.contains(s.charAt(right))){
+            set.add(s.charAt(right));
+            maxLen = Math.max(maxLen,right - left +1);
+            right++;
+        }else{
+            set.remove(s.charAt(left));
+            left++;
+        }
+       }
+        return maxLen;
+    }
 }
 public class Problem16 {
     public static void main(String[] args) {
         Solution s = new Solution();
         String ss = "abcabcbb";
-        System.out.println("Brute Force:"+s.lengthOfLongestSubstring(ss));
+        System.out.println("Brute Force:"+s.lengthOfLongestSubstringBruteForce(ss));
+        System.out.println("Optimal:"+s.lengthOfLongestSubstringOptimal(ss));
     }
 }
