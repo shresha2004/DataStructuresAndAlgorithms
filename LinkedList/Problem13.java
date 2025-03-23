0a1a784fb18c0d7f71dd2fdb22df12049961548a
Shresha Acharya <shreshacharya06@gmail.com>
2025-03-23 23:16:00 +0530

    Count no. of Nodes in a loop of SLL

--- LinkedList/Problem13.java
+++ LinkedList/Problem13.java
@@ -1,5 +1,5 @@
 //Problem:https://www.geeksforgeeks.org/problems/find-length-of-loop/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=find-length-of-loop
-
+import java.util.HashSet;
 class Node {
     int data;
     Node next;
@@ -11,7 +11,29 @@ class Node {
 }
 
 class Solution {
-
+    public int countNodesinLoopBruteForce(Node head) {
+        // Add your code here.
+        if(head == null || head.next == null ) return 0;
+        HashSet<Node> set = new HashSet<>();
+        Node mover = head;
+         while(mover != null){
+             if(!set.contains(mover)) set.add(mover);
+             else return countNode(mover);
+             mover = mover.next;
+         }
+         return 0;
+        
+    }
+    private int countNode(Node start){
+        int count = 1;
+        Node mover = start.next;
+        while(start != mover){
+            mover = mover.next;
+            count++;
+        }
+        return count;
+        
+    }
     public int countNodesinLoopOptimal(Node head) {
         // Add your code here.
         if (head == null || head.next == null)
@@ -56,6 +78,7 @@ public class Problem13 {
         Solution s = new Solution();
         int[] arr = { 25, 14, 19, 33, 10, 21, 39, 90, 58, 45 };
         Node head = addingArrToLL(arr);
+        System.out.println("Brute Force:"+s.countNodesinLoopBruteForce(head));
         System.out.println("Optimal:"+s.countNodesinLoopOptimal(head));
 
     }
