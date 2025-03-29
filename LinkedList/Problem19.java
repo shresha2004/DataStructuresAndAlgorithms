4409f88808289ac22ea373612ded036e1b1bd633
Shresha Acharya <shreshacharya06@gmail.com>
2025-03-29 12:38:39 +0530

    Sort 3 diff eles in SLL

--- LinkedList/Problem19.java
+++ LinkedList/Problem19.java
@@ -58,10 +58,49 @@ class Solution {
         }
         return dummyNode.next;
     }
+
+    public Node segregateBetter(Node head) {
+        // add your code here
+        int count0 =0;
+        int count1 = 0;
+        int count2 = 0;
+        
+        Node temp = head;
+        
+        while(temp != null){
+            if(temp.data == 0) count0++;
+            else if(temp.data == 1) count1++;
+            else count2++;
+            temp = temp.next;
+        }
+        
+        temp = head;
+        while(temp != null){
+            if(count0 > 0){
+                temp.data = 0;
+                count0--;
+                 
+            }
+            else if(count1 > 0){
+                count1--;
+                temp.data = 1;
+                 
+            }
+            else{
+                count2--;
+                temp.data = 2;
+                
+            }
+            temp = temp.next;
+            
+        }
+        return head;
+        
+    }
    
 }
 
-public class Problem {
+public class Problem19 {
      public static Node addingArrToLL(int arr[]){
         Node head = new  Node(arr[0]);
         Node mover = head;
@@ -75,8 +114,12 @@ public class Problem {
     public static void main(String[] args){
         Solution s = new Solution();
         int[] arr = {1,2,2,1,2,0,2,2 };
-        Node head = addingArrToLL(arr);
-        System.out.println("Brute Force:"+s.segregateBruteForce(head).data);
+        Node head1 = addingArrToLL(arr);
+        Node head2 = addingArrToLL(arr);
+        Node head3 = addingArrToLL(arr);
+        System.out.println("Brute Force:"+s.segregateBruteForce(head1).data);
+        System.out.println("Better:"+s.segregateBruteForce(head2).data);
+       // System.out.println("Optimal:"+s.segregateBruteForce(head3).data);
 
     }
 }
