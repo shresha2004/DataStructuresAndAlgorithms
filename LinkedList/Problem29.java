aacfcb417e82125a48ce6fdf323d32efca26556f
Shresha Acharya <shreshacharya06@gmail.com>
2025-04-06 00:06:54 +0530

    Rotate SLL

--- LinkedList/Problem29.java
+++ LinkedList/Problem29.java
@@ -2,15 +2,16 @@
 class ListNode {
     int val;
     ListNode next;
+
     ListNode(int x) {
-    val = x;
-    next=null;
+        val = x;
+        next = null;
     }
 }
 
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if(head == null || head.next == null) return head;
+        if(head == null || head.next == null || k==0) return head;
         int length = 1;
         ListNode mover = head;
         while(mover.next != null){
@@ -18,43 +19,50 @@ class Solution {
             mover = mover.next;
 
         }
-        if(k== 0 || length % k ==0) return head;
-        k = k%length;
         mover.next = head;
+          k = k%length;
+          if(k==0){
+            mover.next = null;
+            return head;
+          }
+
         ListNode newNode = findNode(head,length - k);
         head = newNode.next;
         newNode.next =null;
         return head;
     }
-    private ListNode findNode(ListNode head, int move){
+
+    private ListNode findNode(ListNode head, int move) {
         ListNode mover = head;
         int count = 1;
-        while(mover != null){
-            if(count == move) return mover;
+        while (mover != null) {
+            if (count == move)
+                return mover;
             mover = mover.next;
             count++;
         }
         return mover;
     }
 }
-public class Problem29 {
-   public static ListNode addingArrToLL(int arr[]){
-      ListNode head = new  ListNode(arr[0]);
-      ListNode mover = head;
-      for(int i=1;i<arr.length;i++){
-          ListNode temp = new ListNode(arr[i]);
-          mover.next = temp;
-          mover=temp;
-      }
-      return head;  
-  }
 
-  public static void main(String[] args) {
-      Solution s = new Solution();
-      int[] arr = { };
-     ListNode head= addingArrToLL(arr);
-     
-      System.out.println("Brute Force:"+s.rotateRight(head,2));
-     // System.out.println("Optimal:"+s.);
-  }
+public class Problem29 {
+    public static ListNode addingArrToLL(int arr[]) {
+        ListNode head = new ListNode(arr[0]);
+        ListNode mover = head;
+        for (int i = 1; i < arr.length; i++) {
+            ListNode temp = new ListNode(arr[i]);
+            mover.next = temp;
+            mover = temp;
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] arr = {1,2,3,4,5};
+        ListNode head = addingArrToLL(arr);
+
+        System.out.println("Brute Force:" + s.rotateRight(head, 2).val);
+        // System.out.println("Optimal:"+s.);
+    }
 }
