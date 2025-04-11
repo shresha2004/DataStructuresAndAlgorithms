a738d780dcf96526c12938116fbca44d876a4df2
Shresha Acharya <shreshacharya06@gmail.com>
2025-04-11 16:27:29 +0530

    3 Sum closest

--- Arrays/Problem66.java
+++ Arrays/Problem66.java
@@ -1,5 +1,7 @@
 //Problem:https://leetcode.com/problems/3sum-closest/description/?envType=problem-list-v2&envId=two-pointers
+import java.util.Arrays;
 class Solution {
+
     public int threeSumClosestBruteForce(int[] nums, int target) {
         int n = nums.length;
         int res = Integer.MAX_VALUE;
@@ -14,6 +16,49 @@ class Solution {
         }
         return res;
     }
+    public int threeSumClosestOptimal(int[] nums, int target) {
+        int n= nums.length;
+       quickSort(nums,0,n-1);
+       int res = nums[0]+nums[1]+nums[2];
+        for(int i=0;i<n-2;i++){
+            int left = i+1;
+            int right = n-1;
+            while(left < right){
+                int curSum = nums[i]+nums[left]+nums[right];
+                if(curSum == target) return target;
+                if(Math.abs(curSum-target)<Math.abs(res-target)) res = curSum;
+                if(curSum > target) right--;
+                else left++;
+            }
+        }
+
+       return res;
+    }
+    private void quickSort(int[] nums,int low,int high){
+        if(low < high){
+            int p = partition(nums,low,high);
+            quickSort(nums,low,p-1);
+            quickSort(nums,p+1,high);
+        }
+    }
+    private int partition(int[] nums,int low,int high){
+        int pivot = nums[low];
+        int i=low;
+        int j = high;
+        while(i<=j){
+            while(i<=high && nums[i] <= pivot) i++;
+            while(j>=low && nums[j] > pivot) j--;
+            if(i<j){
+                int temp = nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+            }
+        }
+        int temp = nums[low];
+        nums[low]= nums[j];
+        nums[j]=temp;
+        return j;
+    }
 }
 
 public class Problem66 {
@@ -21,6 +66,6 @@ public class Problem66 {
         Solution s = new Solution();
         int[] arr = {-1,2,1,-4};
         System.out.println("Brute Force:"+s.threeSumClosestBruteForce(arr, 1));
-       // System.out.println("Optimal:"+s.);
+       System.out.println("Optimal:"+s.threeSumClosestOptimal(arr, 1));
     }
 }
