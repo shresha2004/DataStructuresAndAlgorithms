38aff24662119f44a9569e71ebcfc927a79dfcb6
Shresha Acharya <shreshacharya06@gmail.com>
2025-05-03 19:44:37 +0530

    Generating subsets

--- Recurrsion/Problem9.java
+++ Recurrsion/Problem9.java
@@ -6,7 +6,7 @@ class Solution {
     
     public List<List<Integer>> subset= new ArrayList<>();
 
-     public List<List<Integer>> subsets(int[] nums) {
+     public List<List<Integer>> subsetsRecursion(int[] nums) {
         f(0,new ArrayList<Integer>(),nums);
             return subset; 
          }
@@ -21,12 +21,27 @@ class Solution {
          arr.remove(arr.size()-1);
          f(index+1,arr,nums);
      }
+     public List<List<Integer>> subsetsBitManipulation(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        
+        int n = nums.length;
+        for(int num=0;num<(1<<n);num++){
+        List<Integer> set = new ArrayList<>();
+            for(int i=0;i<n;i++){
+                if((num &(1<<i))!=0) set.add(nums[i]);
+            }
+            if(set.size()>0) ans.add(set);
+        }
+       ans.add(new ArrayList<>());
+        return ans;
+    }
      }
 public class Problem9 {
     public static void main(String[] args) {
      Solution s=new Solution();
      int[] num={3,1,2};
-    System.out.println(s.subsets(num)); 
+    System.out.println("Using Recursion:"+s.subsetsRecursion(num));
+    System.out.println("Using Bitmanipulation:"+s.subsetsBitManipulation(num));
     }
     
 }
