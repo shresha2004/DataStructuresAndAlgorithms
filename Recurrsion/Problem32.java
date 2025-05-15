398d38abb49877e9d44f346e0b2fe27dd82bae51
Shresha Acharya <shreshacharya06@gmail.com>
2025-05-15 23:11:39 +0530

    Valid sudoku

---
+++ Recurrsion/Problem32.java
@@ -0,0 +1,60 @@
+//Problem:https://leetcode.com/problems/sudoku-solver/description
+import java.util.Arrays;
+class Solution {
+    public char[][] solveSudoku(char[][] board) {
+        solve(board);
+        return board;
+    }
+
+    private boolean solve(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == '.') {
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (isValid(board, i, j, c)) {
+                            board[i][j] = c;
+
+                            if (solve(board))
+                                return true;
+                            else
+                                board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == c)
+                return false;
+            if (board[row][i] == c)
+                return false;
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
+                return false;
+        }
+        return true;
+    }
+}
+
+public class Problem32 {
+    public static void main(String[] args) {
+        char[][] board = {
+    {'5','3','.','.','7','.','.','.','.'},
+    {'6','.','.','1','9','5','.','.','.'},
+    {'.','9','8','.','.','.','.','6','.'},
+    {'8','.','.','.','6','.','.','.','3'},
+    {'4','.','.','8','.','3','.','.','1'},
+    {'7','.','.','.','2','.','.','.','6'},
+    {'.','6','.','.','.','.','2','8','.'},
+    {'.','.','.','4','1','9','.','.','5'},
+    {'.','.','.','.','8','.','.','7','9'}
+};
+
+        Solution s = new Solution();
+        System.out.println("Recursive:"+(Arrays.toString(s.solveSudoku(board))));
+    }
+}
