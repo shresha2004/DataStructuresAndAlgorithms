53b75b6c6913b4d7ce230679c9861380428787be
Shresha Acharya <shreshacharya06@gmail.com>
2025-05-20 18:33:55 +0530

    Set or not (0 or 1)

--- BitManipulation/Problem1.java
+++ BitManipulation/Problem1.java
@@ -1,24 +1,36 @@
+
 //Problem:https://www.geeksforgeeks.org/problems/check-whether-k-th-bit-is-set-or-not-1587115620/1
 import java.util.ArrayList;
-class Solution{
- static boolean checkKthBit(int n, int k) {
+
+class Solution {
+    static boolean checkKthBitBruteForce(int n, int k) {
         // Your code here
         ArrayList<Integer> lst = new ArrayList<>();
-        while(n !=0){
-            if(n%2== 1) lst.add(1);
-            else lst.add(0);
-           
-            n=n/2;
+        while (n != 0) {
+            if (n % 2 == 1)
+                lst.add(1);
+            else
+                lst.add(0);
+
+            n = n / 2;
         }
-        if(k>=lst.size()) return false;
-        return lst.get(k)==1;
+        if (k >= lst.size())
+            return false;
+        return lst.get(k) == 1;
+    }
+
+    static boolean checkKthBitOptimal(int n, int k) {
+        // Your code here
+        return (n & (1 << k)) != 0;
     }
 }
+
 public class Problem1 {
     public static void main(String[] args) {
         Solution s = new Solution();
-        int num = 4 ;
+        int num = 4;
         int k = 0;
-        System.out.println("Brute Force:"+s.checkKthBit(num,k));
+        System.out.println("Brute Force:" + s.checkKthBitBruteForce(num, k));
+        System.out.println("Optimal:" + s.checkKthBitOptimal(num, k));
     }
 }
