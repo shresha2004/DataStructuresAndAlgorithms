4b3c7ae692060a4e6607cc84ff72bbfb275e2690
Shresha Acharya <shreshacharya06@gmail.com>
2025-05-25 21:56:31 +0530

    Toggle bits using mask

--- BitManipulation/Problem6.java
+++ BitManipulation/Problem6.java
@@ -1,6 +1,6 @@
 //Problem:https://www.geeksforgeeks.org/problems/toggle-bits-given-range0952/1
 class Solution{
- static int toggleBits(int n, int l, int r) {
+ static int toggleBitsBruteForce(int n, int l, int r) {
         // code here
         for(int i=l;i<=r;i++){
             n=(n ^(1<<i-1));
@@ -8,12 +8,18 @@ class Solution{
         }
         return n;
     }
+     static int toggleBitsOptimal(int n, int l, int r) {
+        // code here
+        int mask = (((1<<r)-1) ^ ((1<<(l-1))-1));
+        return n ^ mask;
+    }
 }
 public class Problem6 {
     public static void main(String[] args) {
         Solution s = new Solution();
         int num =17 ;
-        System.out.println("Brute Force:"+s.toggleBits(num, 2, 3));
+        System.out.println("Brute Force:"+s.toggleBitsBruteForce(num, 2, 3));
+        System.out.println("Optimal:"+s.toggleBitsOptimal(num, 2, 3));
     }
 }
 
