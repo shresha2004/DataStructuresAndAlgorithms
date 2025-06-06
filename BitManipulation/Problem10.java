a92102007a14428e669fd0cfb94e843bef91f755
Shresha Acharya <shreshacharya06@gmail.com>
2025-06-07 00:54:52 +0530

    Divide without division,multiplication and modulo operator

--- BitManipulation/Problem10.java
+++ BitManipulation/Problem10.java
@@ -13,12 +13,32 @@ class Solution{
         }
         return (isNegative)? -count:count;
     }
+
+    public int divideOptimal(int dividend, int divisor) {
+         if(dividend == Integer.MIN_VALUE && divisor == -1) return Integer.MAX_VALUE;
+        if (dividend == divisor)
+            return 1;
+        boolean isNegative = (dividend < 0) ^ (divisor < 0);
+        long n = Math.abs((long)dividend);
+        long d = Math.abs((long)divisor);
+        int ans = 0;
+        while (n >= d) {
+            int count = 0;
+            while (n >= (d << (count + 1)))
+                count++;
+            ans += 1 << count;
+            n -= (d << count);
+        }
+        
+        return (isNegative) ? -ans : ans;
+    }
 }
 public class Problem10 {
     public static void main(String[] args) {
         Solution s = new Solution();
       int dividend = 10, divisor = 3;
         System.out.println("Brute Force:"+s.divideBrutForce(dividend, divisor));
+        System.out.println("Optimal:"+s.divideOptimal(dividend, divisor));
     }
 }
 
