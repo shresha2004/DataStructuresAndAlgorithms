f7a03c6d045bd9784aacceea3e38db6ec3643826
Shresha Acharya <shreshacharya06@gmail.com>
2025-06-22 17:55:55 +0530

    Check ugly number

--- BasicMathsForDSA/Problem19.java
+++ BasicMathsForDSA/Problem19.java
@@ -12,11 +12,24 @@ class Solution{
        if(n != 1 && n>5) return false;
        return true;
     }
+
+     public boolean isUglyOptimal(int n) {
+     if(n<=0) return false;
+     int[] factors= {2,3,5};
+
+     for(int f : factors){
+        if(n%f==0){
+            while(n%f == 0) n/=f;
+        }
+     }
+     return n==1;
+    }
 }
 public class Problem19 {
     public static void main(String[] args) {
         Solution s = new Solution();
         int num = 9;
         System.out.println("Brute Force:"+s.isUglyBruteForce(num));
+        System.out.println("Optimal:"+s.isUglyBruteForce(num));
     }
 }
