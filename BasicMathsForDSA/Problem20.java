4d1babd09041e5ee780fc5ce1a39c7fdba7547b5
Shresha Acharya <shreshacharya06@gmail.com>
2025-07-02 18:03:58 +0530

    Find prime factors

--- BasicMathsForDSA/Problem20.java
+++ BasicMathsForDSA/Problem20.java
@@ -24,6 +24,6 @@ public class Problem20 {
         Solution s = new Solution();
         int num = 2000;
         System.out.println("Brute Force:"+s.findPrimeFactorsBruteForce(num));
-        System.out.println("Optimal:");
+     
     }
 }
