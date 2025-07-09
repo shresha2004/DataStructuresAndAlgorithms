0729782f8e446bdccf623158f1e1a8abab765758
Shresha Acharya <shreshacharya06@gmail.com>
2025-07-09 22:46:56 +0530

    prime Factors

--- BasicMathsForDSA/Problem20.java
+++ BasicMathsForDSA/Problem20.java
@@ -18,11 +18,31 @@ static List<Integer> findPrimeFactorsBruteForce(int N) {
         }
         return ans;
     }
+    static List<Integer> findPrimeFactorsOptimal(int N) {
+        // code here
+        int limit = N+1;
+        int[] spf = new int[limit];
+        for(int i=1;i<limit;i++) spf[i]=i;
+        for(int i=2;i*i<limit;i++){
+            if(spf[i]==i){
+                for(int j= i*i;j<limit;j+=i){
+                    if(spf[j]==j) spf[j]=i;
+                }
+            }
+        }
+        ArrayList<Integer> ans = new ArrayList<>();
+        while(N != 1){
+            ans.add(spf[N]);
+            N/= spf[N];
+        }
+        return ans;
+    }
 }
 public class Problem20 {
     public static void main(String[] args) {
         Solution s = new Solution();
         int num = 2000;
         System.out.println("Brute Force:"+s.findPrimeFactorsBruteForce(num)); 
+        System.out.println("Optimal:"+s.findPrimeFactorsBruteForce(num)); 
     }
 }
