8cc2507c7b16d8e4fa06fe32c329ae90ed74263a
Shresha Acharya <shreshacharya06@gmail.com>
2025-07-29 00:16:28 +0530

    Dynamic programming.Fibancci implementation

--- DynamicProgramming/Problem1.java
+++ DynamicProgramming/Problem1.java
@@ -1,5 +1,5 @@
  class Solution {
-    public int fib(int n) {
+    public int fibusingMemo(int n) {
         int[] dp = new int[n+1];
         for(int i=0;i<n+1;i++){
             dp[i]=-1;
@@ -13,10 +13,22 @@
         if(dp[n] != -1) return dp[n];
         return dp[n] = fibUsingDP(n-1,dp)+fibUsingDP(n-2,dp);
     }
+
+     public int fibUsingTabul(int n) {
+        if(n==0 || n==1) return n;
+        int[] dp = new int[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<n+1;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+      return dp[n];
+    }
 }
 public class Problem1{
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println("Dynamic programing approach:"+s.fib(5));
+        System.out.println("Usng memoization:"+s.fibusingMemo(5));
+        System.out.println("Using tabulation:"+s.fibUsingTabul(5));
     }
 }
