639609e83784d56a5bbe07018c67be22305f00b2
Shresha Acharya <shreshacharya06@gmail.com>
2025-07-31 00:50:20 +0530

    Check celebrity

--- Stack&Queue/Problem25.java
+++ Stack&Queue/Problem25.java
@@ -23,11 +23,34 @@ class Solution {
             }
         return -1;
     }
+
+     public int celebrityOptimal(int mat[][]) {
+        // code here
+        int n =mat.length;
+      int top = 0;
+      int down = n-1;
+      
+      while(top < down){
+          if(mat[top][down]==1) top++;
+          else if(mat[down][top]==1) down--;
+          else{
+              top++;
+              down--;
+          }
+      }
+      if(top>down) return -1;
+      for(int i=0;i<n;i++){
+          if(top==i) continue;
+          if(mat[top][i]==1 || mat[i][top]==0) return -1;
+      }
+      return top;
+    }
 }
 public class Problem25 {
     public static void main(String[] args) {
         Solution s = new Solution();
        int[][] mat = {{1, 1, 0}, {0, 1, 0}, {0, 1, 1}};
         System.out.println("Brute Force:"+s.celebrityBruteForce(mat));
+        System.out.println("Optimal:"+s.celebrityOptimal(mat));
     }
 }
