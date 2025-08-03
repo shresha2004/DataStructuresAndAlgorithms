59962f5283d6ee10998ee43c44e03096b7b90236
Shresha Acharya <shreshacharya06@gmail.com>
2025-08-03 18:15:24 +0530

    BSF traversal

---
+++ BinaryTree/Problem4.java
@@ -0,0 +1,42 @@
+//Problem:https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+import java.util.*;   
+ class TreeNode {
+       int val;
+       TreeNode left;
+       TreeNode right;
+       TreeNode() {}
+       TreeNode(int val) { this.val = val; }
+       TreeNode(int val, TreeNode left, TreeNode right) {
+           this.val = val;
+           this.left = left;
+           this.right = right;
+       }
+   }
+   
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> qu = new LinkedList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+                if(root == null) return ans;
+
+        qu.offer(root);
+
+        while(!qu.isEmpty()){
+            int levelNum = qu.size();
+            ArrayList<Integer> subList = new ArrayList<>();
+            for(int i=0;i<levelNum;i++){
+                TreeNode node = qu.poll();
+                subList.add(node.val);
+                if(  node.left != null) qu.offer(node.left);
+                if( node.right != null) qu.offer(node.right);
+                
+                
+            }
+            ans.add(subList);
+        }
+        return ans;
+    }
+}
+public class Problem4 {
+    
+}
