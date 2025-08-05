49a4eb606b9b4ce76aa9b8e46c72854a1f6a7a73
Shresha Acharya <shreshacharya06@gmail.com>
2025-08-05 18:23:46 +0530

    Robing Problem

--- DynamicProgramming/Problem6.java
+++ DynamicProgramming/Problem6.java
@@ -33,6 +33,36 @@ class Solution {
         return memo[n]=Math.max(pick,notPick);
     }
 
+     public int robTabulation(int[] nums) {
+     int n = nums.length;
+     int[] tabu = new int[n];
+     tabu[0]=nums[0];
+     int neg = 0;
+     for(int i=1;i<n;i++){
+        int take = nums[i];
+        if(i>1) take+= tabu[i-2];
+        int notTake = tabu[i-1];
+        tabu[i]=Math.max(take,notTake);
+     }
+     return tabu[n-1];
+    }
+
+     public int robSpaceOpti(int[] nums) {
+     int n = nums.length;
+     int prev1=nums[0];
+     int prev2 = 0;
+     for(int i=1;i<n;i++){
+        int take = nums[i];
+        if(i>1) take+= prev2;
+        int notTake = prev1;
+        int cur =Math.max(take,notTake);
+        prev2=prev1;
+        prev1=cur;
+     }
+     return prev1;
+    }
+    
+
 }
 public class Problem6 {
     public static void main(String[] args) {
@@ -40,5 +70,7 @@ public class Problem6 {
         int[] nums = {1,2,3,1};
         System.out.println("Recursion:"+s.robRecursive(nums));
         System.out.println("Memoization:"+s.robMemo(nums));
+        System.out.println("Tabulation:"+s.robTabulation(nums));
+        System.out.println("Space Optimization:"+s.robSpaceOpti(nums));
     }
 }
