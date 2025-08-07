f3a8ed7d7e54be3844afa0bbd22df15fbffc226d
Shresha Acharya <shreshacharya06@gmail.com>
2025-08-07 19:37:26 +0530

    Post order traversing using loop

--- BinaryTree/Problem4.java
+++ BinaryTree/Problem4.java
@@ -36,6 +36,39 @@ class Solution {
         }
         return ans;
     }
+     public List<Integer> postorderTraversal(TreeNode root) {
+      TreeNode curr = root;
+      Stack<TreeNode> st = new Stack<>();
+
+     List<Integer> ans = new ArrayList<>();
+             if(root == null) return ans;
+
+      while(curr != null || !st.isEmpty()){
+        if(curr != null){
+            st.push(curr);
+            curr = curr.left;
+        }
+        else{
+            TreeNode temp = st.peek().right;
+            if(temp == null){
+                temp = st.peek();
+                st.pop();
+                ans.add(temp.val);
+                while(!st.isEmpty() && temp ==st.peek().right){
+                        
+                        temp = st.peek();
+                        st.pop();
+                        ans.add(temp.val);
+                }
+
+            }
+            else {
+                curr=temp;
+            }
+        }
+      }
+      return ans;
+    }
 }
 public class Problem4 {
     
