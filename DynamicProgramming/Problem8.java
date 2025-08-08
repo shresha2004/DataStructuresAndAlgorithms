5042d053b5894a2162bb64c1d2588520c1dc7e6d
Shresha Acharya <shreshacharya06@gmail.com>
2025-08-08 21:57:06 +0530

    Geek's training 2D Recursive

--- DynamicProgramming/Problem8.java
+++ DynamicProgramming/Problem8.java
@@ -25,11 +25,43 @@ class Solution {
         }
         return maxPoints;
     }
+
+
+
+    public int maximumPointsMemoization(int arr[][]) {
+        // code here
+        int day = arr.length-1;
+        int task = arr[0].length;//Initially u can make any task
+        return findMaximumPointsMemoization(day,task,arr);
+    }
+    
+    private int findMaximumPointsMemoization(int day,int prevTask,int[][] arr){
+        if(day == 0){
+            int max = Integer.MIN_VALUE;
+            for(int i=0;i<arr[0].length;i++){
+                if(i != prevTask){
+                    max = Math.max(arr[0][i],max);
+                }
+            }
+            return max;
+        }
+        int maxPoints= 0;
+        for(int i=0;i<arr[0].length;i++){
+            if(i != prevTask){
+              int  points = arr[day][i]+findMaximumPointsMemoization(day-1,i,arr);
+                maxPoints = Math.max(points,maxPoints);
+            }
+        }
+        return maxPoints;
+    }
 }
+
 public class Problem8 {
     public static void main(String[] args) {
         Solution s = new Solution();
         int[][] arr = {{1, 2, 5}, {3, 1, 1}, {3, 3, 3}};
         System.out.println("Recursive:"+s.maximumPointsRecursive(arr));
+        System.out.println("Memoization:"+s.maximumPointsMemoization(arr));
+        
     }
 }
