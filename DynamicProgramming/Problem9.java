41efc7500f27e1d592283c660834e8267bdc818a
Shresha Acharya <shreshacharya06@gmail.com>
2025-08-09 19:00:21 +0530

    Number of unique path using memoization

--- DynamicProgramming/Problem9.java
+++ DynamicProgramming/Problem9.java
@@ -9,6 +9,24 @@ class Solution {
         int right = countPathsRecursive(row,column-1);
         return left + right;
     }
+
+      public int uniquePathsMemoization(int m, int n) {
+        int[][] memo = new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                memo[i][j]=-1;
+            }
+        }
+        return countPathsMemoization(m-1,n-1,memo);
+    }
+    private int countPathsMemoization(int row,int column,int[][] memo){
+         if(row<0 || column <0) return 0;
+         if(memo[row][column] != -1) return memo[row][column];
+        if(row == 0 && column == 0) return 1;
+        int left = countPathsMemoization(row-1,column,memo);
+        int right = countPathsMemoization(row,column-1,memo);
+        return memo[row][column]=left + right;
+    }
 }
 public class Problem9 {
     public static void main(String[] args) {
@@ -16,5 +34,8 @@ public class Problem9 {
         int m = 3;
         int n= 2;
         System.out.println("Recursive:"+s.uniquePathsRecursive(m, n));
+        System.out.println("Memoization:"+s.uniquePathsMemoization(m, n));
     }
+
+  
 }
