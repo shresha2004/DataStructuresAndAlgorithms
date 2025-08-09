bf2f67d6443b63bc586cba501c5817a5bfc5e5b7
Shresha Acharya <shreshacharya06@gmail.com>
2025-08-09 23:35:21 +0530

    Preorder traversal

--- BinaryTree/Problem1.java
+++ BinaryTree/Problem1.java
@@ -45,7 +45,34 @@ class Solution {
 }
 
 public class Problem1 {
+     public static TreeNode createTree(int[] arr) {
+        if (arr.length == 0) return null;
+        TreeNode root = new TreeNode(arr[0]);
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        int i = 1;
+        while (!q.isEmpty() && i < arr.length) {
+            TreeNode current = q.poll();
+
+            if (i < arr.length) {
+                current.left = new TreeNode(arr[i++]);
+                q.offer(current.left);
+            }
+            if (i < arr.length) {
+                current.right = new TreeNode(arr[i++]);
+                q.offer(current.right);
+            }
+        }
+        return root;
+    }
     public static void main(String[] args) {
-        
+        int[] arr = {1, 2, 3, 4, 5, 6, 7}; 
+        TreeNode root = createTree(arr); 
+
+        Solution sol = new Solution();
+
+        System.out.println("Preorder Iterative: " + sol.preorderTraversalUsingLoop(root));
+        System.out.println("Preorder Recursive: " + sol.preorderTraversalUsingRecu(root));
     }
 }
