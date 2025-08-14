610e4f9fa18af3f38ff1b75b8375f1f135458842
Shresha Acharya <shreshacharya06@gmail.com>
2025-08-14 19:48:15 +0530

    Maximum depth of the tree

---
+++ BinaryTree/Problem6.java
@@ -0,0 +1,57 @@
+ //Problem:
+ import java.util.*; 
+   class TreeNode {
+       int val;
+       TreeNode left;
+       TreeNode right;
+       TreeNode() {}
+       TreeNode(int val) { this.val = val; }
+       TreeNode(int val, TreeNode left, TreeNode right) {
+           this.val = val;
+           this.left = left;
+           this.right = right;
+       }
+   }
+    
+class Solution {
+   public int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return 1+Math.max(left,right);
+    }
+}
+
+public class Problem6 {
+     public static TreeNode createTree(int[] arr) {
+        if (arr.length == 0) return null;
+        TreeNode root = new TreeNode(arr[0]);
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        int i = 1;
+        while (!q.isEmpty() && i < arr.length) {
+            TreeNode current = q.poll();
+
+            if (i < arr.length) {
+                current.left = new TreeNode(arr[i++]);
+                q.offer(current.left);
+            }
+            if (i < arr.length) {
+                current.right = new TreeNode(arr[i++]);
+                q.offer(current.right);
+            }
+        }
+        return root;
+    }
+    public static void main(String[] args) {
+        int[] arr = {3,9,20,15,7}; 
+        TreeNode root = createTree(arr); 
+
+        Solution sol = new Solution();
+        System.out.println("Maximum depth:"+sol.maxDepth(root));
+
+      
+    }
+}
+
