84761818dea8b3ce1ba594e20d15888d66e41f03
Shresha Acharya <shreshacharya06@gmail.com>
2025-08-18 00:43:43 +0530

    Pick chocolates using 3D dp

--- DynamicProgramming/Problem14.java
+++ DynamicProgramming/Problem14.java
@@ -29,6 +29,43 @@ class Solution {
         }
         return max;
         }
+
+         public int solveMemo(int n, int m, int grid[][]) {
+        // Code here
+        int j1=0;
+        int j2=grid[0].length-1;
+        int[][][] memo = new int[n][m][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                for(int k =0;k<m;k++)
+                memo[i][j][k]=-1;
+            }
+        }
+        return findMaxMemo(0,j1,j2,grid,n,m,memo);
+    }
+    
+    private int findMaxMemo(int i,int j1, int j2,int[][] grid,int n, int m,int[][][] memo){
+        
+        if(j1 < 0 || j2 < 0 || j2 >= m || j1 >= m ) return Integer.MIN_VALUE;
+        
+        if(memo[i][j1][j2] != -1) return memo[i][j1][j2];
+        if(i == n-1){
+            if(j1==j2) return grid[i][j1];
+            else return grid[i][j1]+grid[i][j2];
+        } 
+       
+       
+        int max = Integer.MIN_VALUE;
+        for(int dj1 = -1;dj1<2;dj1++){
+            for(int dj2 =-1;dj2<2;dj2++){
+            int ans =0;
+                if(j1 == j2) ans= grid[i][j1]+findMaxMemo(i+1,j1+dj1,j2+dj2,grid,n,m,memo);
+                else ans = (grid[i][j1]+grid[i][j2])+findMaxMemo(i+1,j1+dj1,j2+dj2,grid,n,m,memo);
+                max = Math.max(ans,max);
+    }
+        }
+        return memo[i][j1][j2]=max;
+        }
 }
 public class Problem14 {
     public static void main(String[] args) {
@@ -36,5 +73,6 @@ public class Problem14 {
         int n = 4, m = 3;
      int[][] grid = {{3,1,1},{2,5,1},{1,5,5},{2,1,1}};
         System.out.println("Recursive:"+s.solveRecursive(n,m,grid));
+        System.out.println("Memoization:"+s.solveMemo(n, m, grid));
     }
 }
