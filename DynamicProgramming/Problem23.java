35293ce41dbbe1a835c8c43a011e5115fe5ea382
Shresha Acharya <shreshacharya06@gmail.com>
2025-08-27 23:25:17 +0530

    Unbounded Knapsack using DP

--- DynamicProgramming/Problem23.java
+++ DynamicProgramming/Problem23.java
@@ -32,6 +32,46 @@ class Solution {
         if(wt[index]<= capacity) pick = val[index]+knapSackMemo(val,wt,capacity-wt[index],index,memo);
         return memo[index][capacity]=Math.max(pick,notPick);
     }
+
+     static int knapSackTabu(int val[], int wt[], int capacity) {
+        // code here
+        int[][] tabu = new int[wt.length][capacity+1];
+        for(int c=0;c<=capacity;c++){
+            // c/wt[0] determines how many wt can u fill for that capacity. * val[0] determines value
+            tabu[0][c]= ((int)c/wt[0] *(val[0]));
+        }
+        for(int i=1;i<wt.length;i++){
+            for(int c=0;c<=capacity;c++){
+                int notPick = tabu[i-1][c];
+                int pick = 0;
+              if(wt[i]<= c)   pick = val[i]+tabu[i][c-wt[i]];
+              tabu[i][c]= Math.max(pick,notPick);
+            }
+        }
+        return tabu[wt.length-1][capacity];
+        
+    }
+
+     static int knapSackSpo(int val[], int wt[], int capacity) {
+        // code here
+        int[] spo = new int[capacity+1];
+        for(int c=0;c<=capacity;c++){
+            // c/wt[0] determines how many wt can u fill for that capacity. * val[0] determines value
+            spo[c]= ((int)c/wt[0] *(val[0]));
+        }
+        for(int i=1;i<wt.length;i++){
+            int[] temp = new int[capacity+1];
+            for(int c=0;c<=capacity;c++){
+                int notPick = spo[c];
+                int pick = 0;
+              if(wt[i]<= c)   pick = val[i]+temp[c-wt[i]];
+              temp[c]= Math.max(pick,notPick);
+            }
+            spo= temp;
+        }
+        return spo[capacity];
+        
+    }
    
 }
 public class Problem23 {
@@ -41,5 +81,7 @@ public class Problem23 {
        int capacity = 3;
        System.out.println("Recursive:"+s.knapSackRecursive(val, wt, capacity));
        System.out.println("Memoization:"+s.knapSackMemo(val, wt, capacity));
+       System.out.println("Tabulation:"+s.knapSackTabu(val, wt, capacity));
+       System.out.println("Space Optimization:"+s.knapSackSpo(val, wt, capacity));
     }
 }
