561ccf7a5e4e9e31c1d8e41e2dd8847080da3dd6
Shresha Acharya <shreshacharya06@gmail.com>
2025-12-02 19:17:09 +0530

    String matching

---
+++ Strings/Problem33.java
@@ -0,0 +1,18 @@
+//Problem:https://leetcode.com/problems/shuffle-string/description/
+class Solution {
+    public String restoreString(String s, int[] indices) {
+     char[] ans = new char[indices.length];
+     for(int i=0;i<s.length();i++){
+        ans[indices[i]]=s.charAt(i);
+     }
+     return new String(ans);
+    }
+}
+public class Problem33 {
+    public static void main(String[] args) {
+        String s = "codeleet";
+        int[] indices = {4,5,6,7,0,2,1,3};
+        Solution ss = new Solution();
+        System.out.println("Answer:"+ss.restoreString(s, indices));
+    }
+}
