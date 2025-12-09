93ceacf349ea1258a4d1f46ecf7fd07db3305d25
Shresha Acharya <shreshacharya06@gmail.com>
2025-12-09 22:44:24 +0530

    Sum of 2 numbers

---
+++ BasicMathsForDSA/Problem24.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1+num2;
+    }
+}
+public class Problem24 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println("Solution:"+s.sum(45, 90));
+    }
+}
